a6dcf996fe432b53d971032cd68bccf372da2df2
Alexandra Rollings <alexandrarollings@rocketmail.com>
2016-07-08 18:32:12 +0100

    Added 0.95 scaling factor to k mode width

--- src/Pdf.cpp
+++ src/Pdf.cpp
@@ -4,41 +4,44 @@
 Pdf::Pdf(Bachelor bachelor, Daughters daughters, RooRealVar &fitVariable)
          // RooArgList const &commonFunctions)
     : bachelor_(bachelor), daughters_(daughters),
-      signalMean_("m[Bu]_Mean", "m[Bu] signal mean", 5279, 5275, 5285),
-      signalSigma_("m[Bu]_Sigma", "m[Bu] signal sigma", 18, 15, 23),
+      relativeWidth_("relativeWidth", "Relative width of k w.r.t. pi Pdfs", 0), 
+      signalMean_("signalMean", "m[Bu] signal mean", 5279, 5275, 5285),
+      signalSigma_("signalSigma", "m[Bu] signal sigma", 18, 15, 23),
+      //Use a RooFormulaVar to relate widths of pi and k Pdfs
+      signalSigmaFormula_("signalSigmaFormula", "m[Bu} K signal sigma", "(@0*@1)", RooArgList(signalSigma_,relativeWidth_)),
       aSignal_("aSignal", "a Signal", 1, -2, 5),
       nSignal_("nSignal", "n Signal", 1, 0, 5),
       lambdaCombinatorial_(
           ("lambda_Combinatorial_" + ComposeFittingCategoryName(bachelor_, daughters_)).c_str(),
           "Combinatorial lambda", 0, -0.1, 0.1),
       meanNonTMSignal_("meanNonTMSignal", "Non TM signal mean", 5284.8),
-      sigmaLeftNonTMSignal_("sigmaLeftNonTMSignal", "Non TM signal sigmaLeft", 52.6),
-      sigmaRightNonTMSignal_("sigmaRightNonTMSignal", "Non TM signal sigmaRight", 49.4),
+      sigmaLeftNonTMSignal_(("sigmaLeftNonTMSignal_" + EnumToString(bachelor_)).c_str(), "Non TM signal sigmaLeft", 52.6),
+      sigmaRightNonTMSignal_(("sigmaRightNonTMSignal_" + EnumToString(bachelor_)).c_str(), "Non TM signal sigmaRight", 49.4),
       meanCrossFeed_("meanCrossFeed", "Neutral cross feed mean", 5283),
-      sigmaCrossFeed_("sigmaCrossFeed", "Neutral cross feed sigma", 80),
+      sigmaCrossFeed_(("sigmaCrossFeed_" + EnumToString(bachelor_)).c_str(), "Neutral cross feed sigma", 80),
       aCrossFeed_("aCrossFeed", "aCrossFeed", 3.5),
       nCrossFeed_("nCrossFeed", "nCrossFeed", 2.8),
       meanBu2Dst0Hst_D0pi0_("meanBu2Dst0Hst_D0pi0", "Bu2Dst0Hst_D0pi0 mean", 5080),
-      sigmaBu2Dst0Hst_D0pi0_("sigmaBu2Dst0Hst_D0pi0", "Bu2Dst0Hst_D0pi0 sigma", 57.8),
+      sigmaBu2Dst0Hst_D0pi0_(("sigmaBu2Dst0Hst_D0pi0_" + EnumToString(bachelor_)).c_str(), "Bu2Dst0Hst_D0pi0 sigma", 57.8),
       aBu2Dst0Hst_D0pi0_("aBu2Dst0Hst_D0pi0", "aBu2Dst0Hst_D0pi0", 1.23),
       nBu2Dst0Hst_D0pi0_("nBu2Dst0Hst_D0pi0", "nBu2Dst0Hst_D0pi0", 0.1),
       meanBu2Dst0Hst_D0gamma_("meanBu2Dst0Hst_D0gamma", "Bu2Dst0Hst_D0gamma mean", 5068),
-      sigmaBu2Dst0Hst_D0gamma_("sigmaBu2Dst0Hst_D0gammai", "Bu2Dst0Hst_D0gamma sigma", 91.5),
+      sigmaBu2Dst0Hst_D0gamma_(("sigmaBu2Dst0Hst_D0gamma_" + EnumToString(bachelor_)).c_str(), "Bu2Dst0Hst_D0gamma sigma", 91.5),
       aBu2Dst0Hst_D0gamma_("aBu2Dst0Hst_D0gamma", "aBu2Dst0Hst_D0gamma", 1.0),
       nBu2Dst0Hst_D0gamma_("nBu2Dst0Hst_D0gamma", "nBu2Dst0Hst_D0gamma", 0.1),
       meanBu2D0H_("meanBu2D0H", "Bu2D0H mean", 5492),
-      sigmaLeftBu2D0H_("sigmaLeftBu2D0H", "Bu2D0H sigmaLeft", 35.7),
-      sigmaRightBu2D0H_("sigmaRightBu2D0H", "Bu2D0H sigmaRight", 46.7),
+      sigmaLeftBu2D0H_(("sigmaLeftBu2D0H_" + EnumToString(bachelor_)).c_str(), "Bu2D0H sigmaLeft", 35.7),
+      sigmaRightBu2D0H_(("sigmaRightBu2D0H_" + EnumToString(bachelor_)).c_str(), "Bu2D0H sigmaRight", 46.7),
       meanBu2D0Hst_("meanBu2D0Hst", "Bu2D0Hst mean", 5304),
-      sigmaBu2D0Hst_("sigmaBu2D0Hst", "Bu2D0Hst sigma", 51.9),
+      sigmaBu2D0Hst_(("sigmaBu2D0Hst_" + EnumToString(bachelor_)).c_str(), "Bu2D0Hst sigma", 51.9),
       aBu2D0Hst_("aBu2D0Hst", "aBu2D0Hst", 0.33),
       nBu2D0Hst_("nBu2D0Hst", "nBu2D0Hst", 5.0),
       meanBd2DstH_("meanBd2DstH", "Bd2DstH mean", 5277.6),
-      sigmaBd2DstH_("sigmaBd2DstH", "Bd2DstH sigma", 54.3),
+      sigmaBd2DstH_(("sigmaBd2DstH_" + EnumToString(bachelor_)).c_str(), "Bd2DstH sigma", 54.3),
       aBd2DstH_("aBd2DstH", "aBd2DstH", 4.0),
       nBd2DstH_("nBd2DstH", "nBd2DstH", 6.1),
       meanBd2D0Hst0_("meanBd2D0Hst0", "Bd2D0Hst0 mean", 5225),
-      sigmaBd2D0Hst0_("sigmaBd2D0Hst0", "Bd2D0Hst0 sigma", 107),
+      sigmaBd2D0Hst0_(("sigmaBd2D0Hst0_" + EnumToString(bachelor_)).c_str(), "Bd2D0Hst0 sigma", 107),
       aBd2D0Hst0_("aBd2D0Hst0", "aBd2D0Hst0", 1.21),
       nBd2D0Hst0_("nBd2D0Hst0", "nBd2D0Hst0", 2.8),
       meanMissId1_(("meanMissId1_" + EnumToString(bachelor_)).c_str(), "MissId1 mean", 0),
@@ -50,7 +53,8 @@ Pdf::Pdf(Bachelor bachelor, Daughters daughters, RooRealVar &fitVariable)
       aMissId2_(("aMissId2_" + EnumToString(bachelor_)).c_str(), "aMissId2", 0),
       nMissId2_(("nMissId2_" + EnumToString(bachelor_)).c_str(), "nMissId2", 0),
       fracMissId1_(("fracMissId1_" + EnumToString(bachelor_)).c_str(), "fracMissId1", 0),
-      signal_(("Signal_" + EnumToString(bachelor_)).c_str(), "Signal gaussian pdf", fitVariable, signalMean_, signalSigma_, aSignal_, nSignal_),
+      // Use RooFormulaVar as input to signal pdf. Set constant relating widths below.
+      signal_(("Signal_" + EnumToString(bachelor_)).c_str(), "Signal gaussian pdf", fitVariable, signalMean_, signalSigmaFormula_, aSignal_, nSignal_),
       nonTMSignal_(("nonTMSignal_"+ EnumToString(bachelor_)).c_str(), "Non TM signal gaussian", fitVariable, meanNonTMSignal_, sigmaLeftNonTMSignal_, sigmaRightNonTMSignal_),
       combinatorialExponential_(
           ("Combinatorial_Exponential_" + ComposeFittingCategoryName(bachelor_, daughters_)).c_str(),
@@ -91,6 +95,8 @@ Pdf::Pdf(Bachelor bachelor, Daughters daughters, RooRealVar &fitVariable)
                     "m[Bu] missId yield", 100, 0, 20000),
       yields_(), addPdf_(nullptr) {
 
+  kRelativeWidth_ = 0.95;
+
   if (bachelor == Bachelor::pi) {
     bachelorScaleFactor = 1;
     //Miss-ID backgrounds have different shapes for pi and k modes
@@ -103,6 +109,7 @@ Pdf::Pdf(Bachelor bachelor, Daughters daughters, RooRealVar &fitVariable)
     aMissId2_.setVal(0.50);
     nMissId2_.setVal(1.68);
     fracMissId1_.setVal(0.390);
+    relativeWidth_.setVal(1.0);
   } else {
     // Scale factor given by ratio of BR's
     bachelorScaleFactor = 0.000420 / 0.00518;
@@ -115,6 +122,17 @@ Pdf::Pdf(Bachelor bachelor, Daughters daughters, RooRealVar &fitVariable)
     aMissId2_.setVal(3.80);
     nMissId2_.setVal(13.7);
     fracMissId1_.setVal(0.712);
+    relativeWidth_.setVal(kRelativeWidth_);
+    sigmaLeftNonTMSignal_.setVal(kRelativeWidth_*52.6);
+    sigmaRightNonTMSignal_.setVal(kRelativeWidth_*49.4);
+    sigmaCrossFeed_.setVal(kRelativeWidth_*80);
+    sigmaBu2Dst0Hst_D0pi0_.setVal(kRelativeWidth_*57.8);
+    sigmaBu2Dst0Hst_D0gamma_.setVal(kRelativeWidth_*91.5);
+    sigmaLeftBu2D0H_.setVal(kRelativeWidth_*35.7);
+    sigmaRightBu2D0H_.setVal(kRelativeWidth_*46.7);
+    sigmaBu2D0Hst_.setVal(kRelativeWidth_*51.9);
+    sigmaBd2DstH_.setVal(kRelativeWidth_*54.3);
+    sigmaBd2D0Hst0_.setVal(kRelativeWidth_*107);
   }
 
   if (daughters == Daughters::kpi) {
