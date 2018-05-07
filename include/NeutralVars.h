28816bce07c16f9641d7f0ecc15939797e577894
Alexandra Rollings <alexandra.rollings@cern.ch>
2018-05-07 14:52:09 +0100

    Forgot a file

--- include/NeutralVars.h
+++ include/NeutralVars.h
@@ -15,28 +15,17 @@
 // your
 // class/function (unless you explicitly specialize, see below).
 
-template <Neutral neutral> class NeutralVars {
-
+template <Neutral neutral>
+class NeutralVars {
   // One template specialization == One entirely separate class in practice
   // These will give two different instances:
   //   NeutralVars<Neutral::gamma>::Get()
   //   NeutralVars<Neutral::pi0>::Get()
   // All happens automatically :-)
-  
-public:  
-  static NeutralVars<neutral> &Get() {
-    static NeutralVars<neutral> singleton;
-    return singleton;
-  }
 
-  // If RooShit wasn't so shit we would pass a const reference
-  RooRealVar &meanDeltaSignal() { return meanDeltaSignal_; }
-  RooRealVar &a0MeanBuSignal() { return a0MeanBuSignal_; }
-  RooPolyVar &meanBuSignal() { return meanBuSignal_; }
-
-private:
+ public:
   // Inline Constructor
-  NeutralVars();
+  NeutralVars(int uniqueId_);
   // Inline Destructor - can both be implemented externally too:
   ~NeutralVars() {}
   // Class Human
@@ -50,8 +39,35 @@ private:
   // Invoked when a class is deleted or goes out of scope: good place to reset
   // variables/release dynamically allocated memory
 
+  using This_t = NeutralVars<neutral>;
+
+  // Get() method of PDF now doesn't always return the same PDF, but the same
+  // PDF for the given ID
+  static This_t &Get(int uniqueId_) {
+    static std::map<int, std::shared_ptr<This_t>> singletons;
+    // An iterator to a map is a std::pair<key, value>, so we need to call
+    // i->second to get the value
+    auto it = singletons.find(uniqueId_);  // Check if uniqueId_ already exists
+    if (it == singletons.end()) {
+      // If it doesn't, create it as a new unique_ptr by calling emplace, which
+      // will forward the pointer to the constructor of std::unique_ptr
+      it = singletons.emplace(uniqueId_, std::make_shared<This_t>(uniqueId_))
+               .first;
+    }
+    return *it->second;
+  }
+
+  // If RooShit wasn't so shit we would pass a const reference
+  int uniqueId() { return uniqueId_; }
+  RooRealVar &meanDeltaSignal() { return meanDeltaSignal_; }
+  RooRealVar &a0MeanBuSignal() { return a0MeanBuSignal_; }
+  RooPolyVar &meanBuSignal() { return meanBuSignal_; }
+
+ private:
+
   // Indicate if only used by one neutral
 
+  int uniqueId_;
   RooRealVar meanDeltaSignal_;
   RooRealVar a0MeanBuSignal_;
   RooPolyVar meanBuSignal_;
@@ -63,6 +79,8 @@ private:
 // different
 
 // ...by telling it exactly which specializations we are providing:
-template <> NeutralVars<Neutral::gamma>::NeutralVars();
-template <> NeutralVars<Neutral::pi0>::NeutralVars();
+template <>
+NeutralVars<Neutral::gamma>::NeutralVars(int uniqueId);
+template <>
+NeutralVars<Neutral::pi0>::NeutralVars(int uniqueId);
 // Then we can safely put these in the .cpp-file somewhere to be linked later.
