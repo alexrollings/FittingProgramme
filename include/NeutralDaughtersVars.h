1ed0817d6e5d7d9bbf8499474b67ec6022d8e261
Alexandra Rollings <alexandra.rollings@cern.ch>
2019-02-12 08:47:38 +0000

    Added unique ID to NDVars

--- include/NeutralDaughtersVars.h
+++ include/NeutralDaughtersVars.h
@@ -16,21 +16,43 @@
 
 template <Neutral neutral, Daughters daughters>
 class NeutralDaughtersVars {
-  // One template specialization == One entirely separate class in practice
-  // These will give two different instances:
-  //   DaughtersVars<Daughters::gamma>::Get()
-  //   DaughtersVars<Daughters::pi0>::Get()
-  // All happens automatically :-)
 
  public:
-  static NeutralDaughtersVars<neutral, daughters> &Get() {
-    static NeutralDaughtersVars<neutral, daughters> singleton;
-    return singleton;
+  // Inline Constructor
+  NeutralDaughtersVars(int uniqueId_);
+  // Inline Destructor - can both be implemented externally too:
+  ~NeutralDaughtersVars() {}
+  // Class Human
+  // {
+  // public:
+  //  ~Human(); //Destructor declaration
+  // };
+  //:: is the scoping operator. Destructor definition/implementation
+  // Human::~Human();
+  // { // code }
+  // Invoked when a class is deleted or goes out of scope: good place to reset
+  // variables/release dynamically allocated memory
+
+  using This_t = NeutralDaughtersVars<neutral, daughters>;
+
+  // Get() method of PDF now doesn't always return the same PDF, but the same
+  // PDF for the given ID
+  static This_t &Get(int uniqueId_) {
+    static std::map<int, std::shared_ptr<This_t>> singletons;
+    // An iterator to a map is a std::pair<key, value>, so we need to call
+    // i->second to get the value
+    auto it = singletons.find(uniqueId_);  // Check if uniqueId_ already exists
+    if (it == singletons.end()) {
+      // If it doesn't, create it as a new unique_ptr by calling emplace, which
+      // will forward the pointer to the constructor of std::unique_ptr
+      it = singletons.emplace(uniqueId_, std::make_shared<This_t>(uniqueId_))
+               .first;
+    }
+    return *it->second;
   }
 
   // If RooShit wasn't so shit we would pass a const reference
-  // RooRealVar &N_Dpi() { return N_Dpi_; }
-  // RooFormulaVar &N_Dk() { return N_Dk_; }
+  int uniqueId() { return uniqueId_; }
   RooRealVar &R_CP_Bu2Dst0h_Dst02D0gamma() {
     return R_CP_Bu2Dst0h_Dst02D0gamma_;
   }
@@ -57,16 +79,12 @@ class NeutralDaughtersVars {
   }
 
  private:
-  // When we DO need to specialize certain cases, we can still do that (see
-  // below)...
-  NeutralDaughtersVars();
-  ~NeutralDaughtersVars() {}
-
   // Indicate if only used by one daughters
 
   // N_Dpi is the total Bu2Dst0pi_D0neut events = signal + SCF + CF + missID
   // RooRealVar N_Dpi_;
   // RooFormulaVar N_Dk_;
+  int uniqueId_;
   RooRealVar R_CP_Bu2Dst0h_Dst02D0gamma_;
   RooRealVar R_CP_Bu2Dst0h_Dst02D0pi0_;
   RooRealVar R_CP_Bu2Dst0hst_Dst02D0gamma_;
@@ -82,19 +100,19 @@ class NeutralDaughtersVars {
 
 // ...by telling it exactly which specializations we are providing:
 template <>
-NeutralDaughtersVars<Neutral::pi0, Daughters::kpi>::NeutralDaughtersVars();
+NeutralDaughtersVars<Neutral::pi0, Daughters::kpi>::NeutralDaughtersVars(int uniqueId);
 template <>
-NeutralDaughtersVars<Neutral::pi0, Daughters::kk>::NeutralDaughtersVars();
+NeutralDaughtersVars<Neutral::pi0, Daughters::kk>::NeutralDaughtersVars(int uniqueId);
 template <>
-NeutralDaughtersVars<Neutral::pi0, Daughters::pipi>::NeutralDaughtersVars();
+NeutralDaughtersVars<Neutral::pi0, Daughters::pipi>::NeutralDaughtersVars(int uniqueId);
 template <>
-NeutralDaughtersVars<Neutral::pi0, Daughters::pik>::NeutralDaughtersVars();
+NeutralDaughtersVars<Neutral::pi0, Daughters::pik>::NeutralDaughtersVars(int uniqueId);
 template <>
-NeutralDaughtersVars<Neutral::gamma, Daughters::kpi>::NeutralDaughtersVars();
+NeutralDaughtersVars<Neutral::gamma, Daughters::kpi>::NeutralDaughtersVars(int uniqueId);
 template <>
-NeutralDaughtersVars<Neutral::gamma, Daughters::kk>::NeutralDaughtersVars();
+NeutralDaughtersVars<Neutral::gamma, Daughters::kk>::NeutralDaughtersVars(int uniqueId);
 template <>
-NeutralDaughtersVars<Neutral::gamma, Daughters::pipi>::NeutralDaughtersVars();
+NeutralDaughtersVars<Neutral::gamma, Daughters::pipi>::NeutralDaughtersVars(int uniqueId);
 template <>
-NeutralDaughtersVars<Neutral::gamma, Daughters::pik>::NeutralDaughtersVars();
+NeutralDaughtersVars<Neutral::gamma, Daughters::pik>::NeutralDaughtersVars(int uniqueId);
 // Then we can safely put these in the .cpp-file somewhere to be linked later.
