d99cfcd0b9188d167e2957303fcfe7c65923e85e
Alexandra Rollings <alexandra.rollings@cern.ch>
2019-05-29 16:06:30 +0100

    Added Bu2D0rho BG instead of exp

--- toys_test/SimToy2D_SaveDataSet.cpp
+++ toys_test/SimToy2D_SaveDataSet.cpp
@@ -265,7 +265,7 @@ void PlotCorrMatrix(RooFitResult *result, std::string const &outputDir) {
 void GenerateToys(std::string const &outputDir) {
   int bu_low = 5050;
   int bu_high = 5800;
-  int delta_low = 70;  // 134;
+  int delta_low = 60;  // 134;
   int delta_high = 210;
 
   int bu_nbins = (bu_high - bu_low) / 10;
@@ -346,16 +346,55 @@ void GenerateToys(std::string const &outputDir) {
 
   // ---------------------------- Background ----------------------------
   //
-  RooRealVar lambdaDeltaBkg("lambdaDeltaBkg", "", 0.01, -0.1, 0.1);
-  RooExponential pdfDeltaBkg("pdfDeltaBkg", "", deltaMass, lambdaDeltaBkg);
+  // RooRealVar lambdaDeltaBkg("lambdaDeltaBkg", "", 0.01, -0.1, 0.1);
+  // RooExponential pdfDeltaBkg("pdfDeltaBkg", "", deltaMass, lambdaDeltaBkg);
+
+  RooRealVar thresholdDeltaBkg("thresholdDeltaBkg", "", 4.7048e+01);
+  RooRealVar cDeltaBkg("cDeltaBkg", "", 6.2583e+01);
+  RooRealVar aDeltaBkg("aDeltaBkg", "", 8.1939e-01);
+  RooRealVar bDeltaBkg("bDeltaBkg", "", -7.9655e-01);
+  RooDstD0BG pdfDeltaBkg("pdfDeltaBkg", "", deltaMass, thresholdDeltaBkg,
+                         cDeltaBkg, aDeltaBkg, bDeltaBkg);
   // ---------------------------- π/K shared PDFs: Bu
   // ----------------------------
-  RooRealVar lambdaBuBkg("lambdaBuBkg", "", -0.005, -0.1, 0.1);
-  RooExponential pdfBuBkg("pdfBuBkg", "", buMass, lambdaBuBkg);
+  // RooRealVar lambdaBuBkg("lambdaBuBkg", "", -0.005, -0.1, 0.1);
+  // RooExponential pdfBuBkg("pdfBuBkg", "", buMass, lambdaBuBkg);
 
-  // ---------------------------- π/K shared Total PDF
-  // ----------------------------
-  RooProdPdf pdfBkg("pdfBkg", "", RooArgSet(pdfDeltaBkg, pdfBuBkg));
+  RooRealVar a0Mean1BuBkg("a0Mean1BuBkg", "",
+                          5.1083e+03);  //, 4500, 5500);
+  RooRealVar a1Mean1BuBkg("a1Mean1BuBkg", "",
+                          2.4092e+00);  //, -10, 10);
+  RooRealVar a2Mean1BuBkg("a2Mean1BuBkg", "",
+                          -5.4048e-03);  //, -0.1, 0.1);
+  RooPolyVar mean1BuBkg("mean1BuBkg", "", deltaMass,
+                        RooArgList(a0Mean1BuBkg, a1Mean1BuBkg, a2Mean1BuBkg));
+  RooRealVar a0Mean2BuBkg("a0Mean2BuBkg", "",
+                          5.0444e+03);  //, -4500, 5500);
+  RooRealVar a1Mean2BuBkg("a1Mean2BuBkg", "",
+                          2.0779e+00);  //, -10, 10);
+  RooRealVar a2Mean2BuBkg("a2Mean2BuBkg", "",
+                          -6.3321e-03);  //, -0.1, 0.1);
+  RooPolyVar mean2BuBkg("mean2BuBkg", "", deltaMass,
+                        RooArgList(a0Mean2BuBkg, a1Mean2BuBkg, a2Mean2BuBkg));
+  // ---------------------------- Sigmas ----------------------------
+  RooRealVar a0Sigma1BuBkg("a0Sigma1BuBkg", "", 9.8314e+00);   //, -300, 300);
+  RooRealVar a1Sigma1BuBkg("a1Sigma1BuBkg", "", 5.8179e-01);   //, -10, 10);
+  RooRealVar a2Sigma1BuBkg("a2Sigma1BuBkg", "", -7.5647e-04);  //, -0.1, 0.1);
+  RooPolyVar sigma1BuBkg(
+      "sigma1BuBkg", "", deltaMass,
+      RooArgList(a0Sigma1BuBkg, a1Sigma1BuBkg, a2Sigma1BuBkg));
+  RooRealVar sigmaFracBuBkg("sigmaFracBuBkg", "", 8.0270e-01);  //, -0.1, 0.1);
+  RooFormulaVar sigma2BuBkg("sigma2BuBkg", "", "@0*@1",
+                            RooArgSet(sigma1BuBkg, sigmaFracBuBkg));
+
+  RooRealVar fracPdf1BuBkg("fracPdf1BuBkg", "", 5.2225e-01);
+  RooGaussian pdfBuBkg1("pdfBuBkg1", "", buMass, mean1BuBkg, sigma1BuBkg);
+  RooGaussian pdfBuBkg2("pdfBuBkg2", "", buMass, mean2BuBkg, sigma2BuBkg);
+  RooAddPdf pdfBuBkg("pdfBuBkg", "", RooArgSet(pdfBuBkg1, pdfBuBkg2),
+                     fracPdf1BuBkg);
+  // ---------------------------- π Total PDF ----------------------------
+  RooProdPdf pdfBkg("pdfBkg", "", pdfDeltaBkg,
+                    RooFit::Conditional(pdfBuBkg, buMass));
 
   // ---------------------------- Yields ----------------------------
   RooRealVar yieldSignal("yieldSignal", "", 40000, -1000000, 1000000);
