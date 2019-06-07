7680266e696d33d1387ffca3f61989a33d6d80c1
Alexandra Rollings <alexandra.rollings@cern.ch>
2019-06-07 11:59:22 +0100

    axis labels formatted to have - not _ between numbers

--- toys_test/BoxCutProfiles.cpp
+++ toys_test/BoxCutProfiles.cpp
@@ -34,6 +34,16 @@ std::vector<std::string> SplitByComma(std::string const &str) {
   return stringVector;
 }
 
+bool fexists(std::string const &filename) {
+  std::ifstream infile(filename.c_str());
+  return infile.is_open();
+}
+
+std::string formatString(std::string s) {
+  std::replace(s.begin(), s.end(), '_', '-');
+  return s;
+}
+
 int main(int argc, char *argv[]) {
   if (argc < 4) {
     std::cerr << "Pass path to input and output directories, and comma "
@@ -50,16 +60,15 @@ int main(int argc, char *argv[]) {
 
   gStyle->SetTitleFont(132, "XYZ");
   gStyle->SetLabelFont(132, "XYZ");
-  gStyle->SetTitleSize(0.06, "Z");
-  gStyle->SetTitleSize(0.04, "XY");
-  gStyle->SetLabelSize(0.03, "Y");
+  gStyle->SetTitleSize(0.03, "XYZ");
+  gStyle->SetLabelSize(0.03, "YZ");
   gStyle->SetLabelSize(0.035, "X");
-  gStyle->SetTitleOffset(0.9, "X");
-  gStyle->SetTitleOffset(0.9, "Y");
-  gStyle->SetTitleOffset(0.9, "Z");
-  gStyle->SetPadTopMargin(0.1);
-  gStyle->SetPadRightMargin(0.03);
-  gStyle->SetPadBottomMargin(0.1);
+  gStyle->SetTitleOffset(1.5, "X");
+  gStyle->SetTitleOffset(1.5, "Y");
+  gStyle->SetTitleOffset(1.5, "Z");
+  gStyle->SetPadTopMargin(0.03);
+  gStyle->SetPadRightMargin(0.15);
+  gStyle->SetPadBottomMargin(0.11);
   gStyle->SetPadLeftMargin(0.1);
 
   TH2D yieldHist("yieldHist", "", bu_bins, 0, bu_bins, delta_bins, 0,
@@ -73,60 +82,98 @@ int main(int argc, char *argv[]) {
   double nFPD = 0;
   double nMINOS = 0;
 
+  double yieldMin = -1000000.0;
+  double yieldMax = 1000000.0;
+  double errMin = -1000000.0;
+  double errMax = 1000000.0;
+  double percErrMin = -1000000.0;
+  double percErrMax = 1000000.0;
+
   for (unsigned int d = 0; d < delta_bins; ++d) {
     for (unsigned int b = 0; b < delta_bins; ++b) {
-      std::string filename = path + "/1d_results/Result_" + delta_low_high[d] +
-                             "_" + bu_low_high[b] + ".root";
+      std::string resultName =
+          "Result_" + delta_low_high[d] + "_" + bu_low_high[b];
+      std::string fileName = path + "/1d_results/" + resultName + ".root";
       // Open file
-      auto file = std::unique_ptr<TFile>(TFile::Open(filename.c_str()));
-      // Extract RooFitResult
-      auto result = std::unique_ptr<RooFitResult>(
-          dynamic_cast<RooFitResult *>(file->FindObjectAny(filename.c_str())));
-      // Check exists
-      if (result == nullptr) {
-        throw std::runtime_error("Could not extract result from " + filename);
-      } else {
-        std::cout << "Extracted result from " << filename << "\n";
-        // Extract tree containing true error
-        auto tree = std::unique_ptr<TTree>(
-            dynamic_cast<TTree *>(file->FindObjectAny("tree")));
+      if (fexists(fileName)) {
+        auto file = std::unique_ptr<TFile>(TFile::Open(fileName.c_str()));
+        // Extract RooFitResult
+        auto result =
+            std::unique_ptr<RooFitResult>(dynamic_cast<RooFitResult *>(
+                file->FindObjectAny(resultName.c_str())));
         // Check exists
-        if (tree == nullptr) {
-          throw std::runtime_error("Could not extract tree from " + filename);
+        if (result == nullptr) {
+          throw std::runtime_error("Could not extract result from " + fileName);
         } else {
-          std::cout << "Extracted tree from " << filename << "\n";
-          // Check fit quality
-          double fitStatus = result->status();
-          double covQual = result->covQual();
-          if (covQual < 2) {
-            nUnConv++;
-          } else if (covQual < 3) {
-            nFPD++;
-          } else if (fitStatus != 0) {
-            nMINOS++;
+          std::cout << "Extracted result from " << fileName << "\n";
+          // Extract tree containing true error
+          auto tree = std::unique_ptr<TTree>(
+              dynamic_cast<TTree *>(file->FindObjectAny("tree")));
+          // Check exists
+          if (tree == nullptr) {
+            throw std::runtime_error("Could not extract tree from " + fileName);
           } else {
-            // Extract total yield from result
-            RooArgList finalPars = result->floatParsFinal();
-            auto yieldAbsArg =
-                std::unique_ptr<RooAbsArg>(finalPars.find("yieldTotSignal"));
-            auto yieldRealVar = std::unique_ptr<RooRealVar>(
-                dynamic_cast<RooRealVar *>(yieldAbsArg.get()));
-            double yieldVal = yieldRealVar->getVal();
+            std::cout << "Extracted tree from " << fileName << "\n";
+            // Check fit quality
+            double fitStatus = result->status();
+            double covQual = result->covQual();
+            if (covQual < 2) {
+              nUnConv++;
+            } else if (covQual < 3) {
+              nFPD++;
+            } else if (fitStatus != 0) {
+              nMINOS++;
+            } else {
+              // Extract total yield from result
+              RooArgList finalPars = result->floatParsFinal();
+              RooAbsArg *yieldAbsArg = finalPars.find("yieldTotSignal");
+              RooRealVar *yieldRealVar =
+                  dynamic_cast<RooRealVar *>(yieldAbsArg);
+              double yieldVal = yieldRealVar->getVal();
 
-            // Get corrected error from tree
-            double yieldErr;
-            tree->SetBranchAddress("errYieldTotSignal", &yieldErr);
-            tree->GetEntry(0);
+              // Get corrected error from tree
+              double yieldErr;
+              tree->SetBranchAddress("errYieldTotSignal", &yieldErr);
+              tree->GetEntry(0);
 
-            // Fill histograms with x and y axis labelled with box dimensions
-            yieldHist.Fill(bu_low_high[b].c_str(),
-                           delta_low_high[d].c_str(), yieldVal);
-            errHist.Fill(bu_low_high[b].c_str(), delta_low_high[d].c_str(),
-                         yieldErr);
-            percErrHist.Fill(bu_low_high[b].c_str(), delta_low_high[d].c_str(),
-                             yieldVal / yieldErr * 100);
+              // Replace _ with - in window string for axis labels
+              std::string xLabel = formatString(bu_low_high[b]);
+              std::string yLabel = formatString(delta_low_high[d]);
+              // Fill histograms with x and y axis labelled with box dimensions
+              yieldHist.Fill(xLabel.c_str(), yLabel.c_str(),
+                             yieldVal);
+              errHist.Fill(xLabel.c_str(), yLabel.c_str(),
+                           yieldErr);
+              double percErr = yieldVal / yieldErr * 100;
+              percErrHist.Fill(xLabel.c_str(),
+                               yLabel.c_str(),
+                               percErr);
+
+              // Set max and min values in order to set z range in TH2s
+              // (otherwise starts from 0)
+              if (yieldVal > yieldMax) {
+                yieldMax = yieldVal;
+              }
+              if (yieldVal < yieldMin) {
+                yieldMin = yieldVal;
+              }
+              if (yieldErr > errMax) {
+                errMax = yieldErr;
+              }
+              if (yieldErr < errMin) {
+                errMin = yieldErr;
+              }
+              if (percErr > percErrMax) {
+                percErrMax = percErr;
+              }
+              if (percErr < percErrMin) {
+                percErrMin = percErr;
+              }
+            }
           }
         }
+      } else {
+        std::cout << fileName << " does not exist\n";
       }
     }
   }
@@ -135,40 +182,50 @@ int main(int argc, char *argv[]) {
             << "Unconverged: " << nUnConv / bu_bins * delta_bins * 100 << " %\n"
             << "Forced positive definite: " << nFPD / bu_bins * delta_bins * 100
             << " %\n"
-            << "MINOS problems: " << nMINOS / bu_bins * delta_bins * 100 << " %\n";
+            << "MINOS problems: " << nMINOS / bu_bins * delta_bins * 100
+            << " %\n";
 
-  TCanvas yieldCanvas("yieldCanvas", "", 1000, 1000);
+  TCanvas yieldCanvas("yieldCanvas", "", 1500, 1000);
 
   yieldHist.GetXaxis()->SetTitle("(m[B^{#pm}] - m[D^{*0}]) Window MeV/c^{2}");
   yieldHist.GetYaxis()->SetTitle("(m[D^{*0}] - m[D^{0}]) Window MeV/c^{2}");
-  yieldHist.GetZaxis()->SetTitle("Yield");
+  yieldHist.GetZaxis()->SetTitle("Signal Yield");
+  double yieldRange = yieldMax - yieldMin;
+  yieldHist.GetZaxis()->SetRangeUser(yieldMin - yieldRange / 5,
+                                     yieldMax + yieldRange / 5);
   yieldHist.SetTitle(" ");
   yieldHist.SetStats(0);
   yieldHist.Draw("colz");
 
-  yieldCanvas.SaveAs((path + "YieldBoxScan.pdf").c_str());
+  yieldCanvas.SaveAs((path + "/YieldBoxScan.pdf").c_str());
 
-  TCanvas errCanvas("errCanvas", "", 1000, 1000);
+  TCanvas errCanvas("errCanvas", "", 1500, 1000);
 
   errHist.GetXaxis()->SetTitle("(m[B^{#pm}] - m[D^{*0}]) Window MeV/c^{2}");
   errHist.GetYaxis()->SetTitle("(m[D^{*0}] - m[D^{0}]) Window MeV/c^{2}");
-  errHist.GetZaxis()->SetTitle("Error");
+  errHist.GetZaxis()->SetTitle("Signal Yield Error");
+  double errRange = errMax - errMin;
+  errHist.GetZaxis()->SetRangeUser(errMin - errRange / 5,
+                                   errMax + errRange / 5);
   errHist.SetTitle(" ");
   errHist.SetStats(0);
   errHist.Draw("colz");
 
-  errCanvas.SaveAs((path + "ErrBoxScan.pdf").c_str());
+  errCanvas.SaveAs((path + "/ErrBoxScan.pdf").c_str());
 
-  TCanvas percErrCanvas("percErrCanvas", "", 1000, 1000);
+  TCanvas percErrCanvas("percErrCanvas", "", 1500, 1000);
 
   percErrHist.GetXaxis()->SetTitle("(m[B^{#pm}] - m[D^{*0}]) Window MeV/c^{2}");
   percErrHist.GetYaxis()->SetTitle("(m[D^{*0}] - m[D^{0}]) Window MeV/c^{2}");
-  percErrHist.GetZaxis()->SetTitle("\% Error");
+  percErrHist.GetZaxis()->SetTitle("Signal Yield \% Error");
+  double percErrRange = percErrMax - percErrMin;
+  percErrHist.GetZaxis()->SetRangeUser(percErrMin - percErrRange / 5,
+                                       percErrMax + percErrRange / 5);
   percErrHist.SetTitle(" ");
   percErrHist.SetStats(0);
   percErrHist.Draw("colz");
 
-  percErrCanvas.SaveAs((path + "percErrBoxScan.pdf").c_str());
+  percErrCanvas.SaveAs((path + "/PercErrBoxScan.pdf").c_str());
 
   return 1;
 }
