036a4827abe7891dc4907179a412602239e9522a
Alexandra Rollings <alexandra.rollings@cern.ch>
2019-06-07 09:56:14 +0100

    Pass box dimensions (unique)

--- toys_test/read_in_box_files.py
+++ toys_test/read_in_box_files.py
@@ -1,32 +1,27 @@
 import os, re, subprocess, sys
 #re = regular expressions
 
-def pass_info(filename, file_list, delta_low, delta_high, bu_low, bu_high):
-    m = re.search("Result_([0-9]+)_([0-9]+)_([0-9]+)_([0-9]+).root", filename)
-    if m:
-        file_list.append(filename)
-        delta_low.append(m.group(1))
-        delta_high.append(m.group(2))
-        bu_low.append(m.group(3))
-        bu_high.append(m.group(4))
 
-if os.path.isdir(sys.argv[1] + "/1d_results"):
-    output_dir = sys.argv[1]
-    file_list = []
-    delta_low = []
-    delta_high = []
-    bu_low = []
-    bu_high = []
-    for filename in os.listdir(sys.argv[1] + "/1d_results"):
-        pass_info(sys.argv[1] + "/" + filename, file_list, delta_low,
-                      delta_high, bu_low, bu_high)
-    subprocess.call([
-        "./BoxCutProfiles", ",".join(file_list), output_dir,
-        ",".join(delta_low), ",".join(delta_high), ",".join(bu_low),
-        ",".join(bu_high)
-    ])
-    # print("./BoxCutProfiles" + " " + ",".join(file_list) + " " + output_dir +
-    #       " " + ",".join(delta_low) + " " + ",".join(delta_high) + " " +
-    #       ",".join(bu_low) + " " + ",".join(bu_high))
+def pass_info(filename, delta_low_high, bu_low_high):
+    m = re.search("Result_([0-9]+_[0-9]+)_([0-9]+_[0-9]+).root", filename)
+    if m:
+        delta_low_high.add(m.group(1))
+        bu_low_high.add(m.group(2))
+
+
+path = sys.argv[1]
+if os.path.isdir(path + "/1d_results"):
+    output_dir = path
+    # Unique box dimensions: unordered set
+    delta_low_high = set()
+    bu_low_high = set()
+    for filename in os.listdir(path + "/1d_results"):
+        pass_info(path + "/" + filename, delta_low_high, bu_low_high)
+    # subprocess.call([
+    #     "./BoxCutProfiles", path, ",".join(delta_low_high),
+    #     ",".join(bu_low_high)
+    # ])
+    print("./BoxCutProfiles " + path + " " + ",".join(delta_low_high) + " " +
+          ",".join(bu_low_high))
 else:
-    print(sys.argv[1] + "/1d_results is not a directory")
+    print(path + "/1d_results is not a directory")
