dd8dc535ae4db828450eb3ec699285afe128b55f
Alexandra Rollings <alexandra.rollings@cern.ch>
2019-07-05 15:33:29 +0200

    Looping over files and extracting RooFitResults using pyRoot

--- toys_test/plotting.py
+++ toys_test/plotting.py
@@ -1,25 +1,55 @@
-import matplotlib.pyplot as plt
+import math, re, os, sys, glob
+from ROOT import TFile, RooFitResult, RooGaussian, RooDataSet
 import numpy as np
-import math
+import matplotlib.pyplot as plt
+import matplotlib.mlab as mlab
+from matplotlib import rc
+from scipy import stats
 
-fig = plt.figure()
-total_yield = 40000
-# x = shared yield
-x = np.arange(0, total_yield, 2000)
-# print(x)
-xerr = np.sqrt(x)
-# print(xerr)
-# y = error correction (corrected error / fit error)
-# y = pull width of total signal yield
-y = x*(math.sqrt(2)/total_yield) + (total_yield - x)/total_yield
-# print(y)
-# Overlay with function:
-# f(x) = x*(math.sqrt(2)/total_yield) + (total_yield - x)/total_yield
-# Propagate uncertainties
+path = sys.argv[1]
 
-plt.errorbar(x, y, xerr=xerr)
-# Double events in box yield or single?
-plt.xlabel('Box yield')
-plt.ylabel('Pull width')
-plt.show()
+bu_low = 5150
+bu_high = []
+delta_low = 110
+delta_high = 210
+
+file_list = []
+
+if os.path.isdir(path):
+  for filename in os.listdir(path):
+    m = re.search("Result_[0-9]+_[0-9]+_[0-9]+_([0-9]+).root", filename)
+    if m:
+        bu_high.append(m.group(1))
+        file_list.append(filename)
+
+cwd = os.getcwd()
+os.chdir(path)
+pars_fit = []
+
+for f in file_list:
+  tf = TFile(filename)
+  result = tf.Get("Result_Pull_yieldTotSignal")
+  pars_fit = result.floatParsFinal()
+  print(pars_fit)
+
+# fig = plt.figure()
+# total_yield = 40000
+# # x = shared yield
+# x = np.arange(0, total_yield, 2000)
+# # print(x)
+# xerr = np.sqrt(x)
+# # print(xerr)
+# # y = error correction (corrected error / fit error)
+# # y = pull width of total signal yield
+# y = x*(math.sqrt(2)/total_yield) + (total_yield - x)/total_yield
+# # print(y)
+# # Overlay with function:
+# # f(x) = x*(math.sqrt(2)/total_yield) + (total_yield - x)/total_yield
+# # Propagate uncertainties
+#
+# plt.errorbar(x, y, xerr=xerr)
+# # Double events in box yield or single?
+# plt.xlabel('Box yield')
+# plt.ylabel('Pull width')
+# plt.show()
 
