c061bcda3c1a194622a3cde52bf2e170bbf39c3e
Alexandra Rollings <alexandra.rollings@cern.ch>
2019-09-18 15:19:12 +0100

    Fix RHS of misRec in K mode (taking up misId yields)

--- src/Pi0PiVars.cpp
+++ src/Pi0PiVars.cpp
@@ -316,7 +316,8 @@ NeutralBachelorVars<Neutral::pi0, Bachelor::pi>::NeutralBachelorVars(
           new RooRealVar(("Bu2D0h_sigmaRBu_" +
                           ComposeName(uniqueId, Neutral::pi0, Bachelor::pi))
                              .c_str(),
-                         "", 4.0080e+01, 20, 60)),  // 7.9369e+01)),
+                             // FIXED TO WHAT DATA FIT FOUND
+                         "", 5.0637e+01)),//4.0080e+01, 20, 60)),  // 7.9369e+01)),
       pdfBu_Bu2D0h_(("pdfBu_Bu2D0h_" +
                      ComposeName(uniqueId, Neutral::pi0, Bachelor::pi))
                         .c_str(),
