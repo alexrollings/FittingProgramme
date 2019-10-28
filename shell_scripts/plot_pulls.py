dd73e681df7775090b0378fe74d6fbf5918cbd18
Alexandra Rollings <alexandra.rollings@cern.ch>
2019-10-28 12:30:24 +0000

    Add batch scripts for plotting pulls too

--- shell_scripts/plot_pulls.py
+++ shell_scripts/plot_pulls.py
@@ -30,18 +30,6 @@ if __name__ == "__main__":
         help='Directory where results should be stored',
         required=True)
     parser.add_argument(
-        '-t',
-        '--n_toys',
-        type=int,
-        help='Number of toy datasets to generate per job',
-        required=True)
-    parser.add_argument(
-        '-j',
-        '--n_jobs',
-        type=int,
-        help='Number of jobs to run on batch for each box dimn',
-        required=True)
-    parser.add_argument(
         '-i',
         '--input_dir',
         type=str,
@@ -62,8 +50,6 @@ if __name__ == "__main__":
 
     neutral = args.neutral
     output_dir = args.output_dir
-    n_toys = args.n_toys
-    n_jobs = args.n_jobs
     input_dir = args.input_dir
     var = args.var
 
@@ -84,7 +70,7 @@ if __name__ == "__main__":
         os.mkdir(output_dir)
 
     home_path = '/home/rollings/Bu2Dst0h_2d/FittingProgramme/'
-    templatePath = home_path + 'shell_scripts/run_box_scan.sh.tmpl'
+    templatePath = home_path + 'shell_scripts/plot_pulls.sh.tmpl'
 
     for neutral in neutrals:
         lines = [l.rstrip('\n') for l in open(home_path + 'shell_scripts/box_effs/' + neutral + '_' + var + '_box_limits.txt')]
@@ -99,32 +85,27 @@ if __name__ == "__main__":
             delta_low = box[2]
             delta_high = box[3]
 
-            for i in range(0, n_jobs):
-                scriptPath = '/data/lhcb/users/rollings/fitting_scripts/tmp/run_box_scan_' + neutral + "_" + delta_low + "_" + delta_high + "_" + bu_low + "_" + bu_high + "_"  + str(i) + ".sh"
-                substitutions = {
-                    "nJob":
-                    i,
-                    "GCCROOT":
-                    "/cvmfs/lhcb.cern.ch/lib/lcg/releases/LCG_88/gcc/4.9.3/x86_64-slc6",
-                    "LD_LIBRARY_PATH":
-                    "/home/rollings/Software/install/lib64:/home/rollings/Software/install/lib:/cvmfs/lhcb.cern.ch/lib/lcg/releases/gcc/4.9.3/x86_64-slc6/lib64:/cvmfs/lhcb.cern.ch/lib/lcg/releases/gcc/4.9.3/x86_64-slc6/lib:/home/rollings/Software/install/lib:64:/cvmfs/lhcb.cern.ch/lib/lcg/releases/gcc/4.9.3/x86_64-slc6/lib64:/cvmfs/lhcb.cern.ch/lib/lcg/releases/gcc/4.9.3/x86_64-slc6/lib::/cvmfs/lhcb.cern.ch/lib/lcg/releases/LCG_88/gcc/4.9.3/x86_64-slc6/lib:/cvmfs/lhcb.cern.ch/lib/lcg/releases/LCG_88/gcc/4.9.3/x86_64-slc6/lib64:/cvmfs/lhcb.cern.ch/lib/lcg/releases/LCG_88/GSL/2.1/x86_64-slc6-gcc49-opt/lib/:/cvmfs/lhcb.cern.ch/lib/lcg/releases/LCG_88/gcc/4.9.3/x86_64-slc6/lib:/cvmfs/lhcb.cern.ch/lib/lcg/releases/LCG_88/gcc/4.9.3/x86_64-slc6/lib64:/cvmfs/lhcb.cern.ch/lib/lcg/releases/LCG_88/GSL/2.1/x86_64-slc6-gcc49-opt/lib/",
-                    "PATH":
-                    output_dir,
-                    "NEUTRAL":
-                    neutral,
-                    "NTOYS":
-                    n_toys,
-                    "INPUT":
-                    input_dir,
-                    "DL":
-                    delta_low,
-                    "DH":
-                    delta_high,
-                    "BL":
-                    bu_low,
-                    "BH":
-                    bu_high
-                }
-                make_shell_script(templatePath, scriptPath, substitutions)
-                # run_process(["sh", scriptPath])
-                run_process(["qsub", scriptPath])
+            scriptPath = '/home/rollings/Bu2Dst0h_2d/FittingProgramme/shell_scripts/tmp/plot_pulls_' + neutral + "_" + delta_low + "_" + delta_high + "_" + bu_low + "_" + bu_high + ".sh"
+            substitutions = {
+                "GCCROOT":
+                "/cvmfs/lhcb.cern.ch/lib/lcg/releases/LCG_88/gcc/4.9.3/x86_64-slc6",
+                "LD_LIBRARY_PATH":
+                "/home/rollings/Software/install/lib64:/home/rollings/Software/install/lib:/cvmfs/lhcb.cern.ch/lib/lcg/releases/gcc/4.9.3/x86_64-slc6/lib64:/cvmfs/lhcb.cern.ch/lib/lcg/releases/gcc/4.9.3/x86_64-slc6/lib:/home/rollings/Software/install/lib:64:/cvmfs/lhcb.cern.ch/lib/lcg/releases/gcc/4.9.3/x86_64-slc6/lib64:/cvmfs/lhcb.cern.ch/lib/lcg/releases/gcc/4.9.3/x86_64-slc6/lib::/cvmfs/lhcb.cern.ch/lib/lcg/releases/LCG_88/gcc/4.9.3/x86_64-slc6/lib:/cvmfs/lhcb.cern.ch/lib/lcg/releases/LCG_88/gcc/4.9.3/x86_64-slc6/lib64:/cvmfs/lhcb.cern.ch/lib/lcg/releases/LCG_88/GSL/2.1/x86_64-slc6-gcc49-opt/lib/:/cvmfs/lhcb.cern.ch/lib/lcg/releases/LCG_88/gcc/4.9.3/x86_64-slc6/lib:/cvmfs/lhcb.cern.ch/lib/lcg/releases/LCG_88/gcc/4.9.3/x86_64-slc6/lib64:/cvmfs/lhcb.cern.ch/lib/lcg/releases/LCG_88/GSL/2.1/x86_64-slc6-gcc49-opt/lib/",
+                "INPUT":
+                input_dir + '/' + var + '/results/',
+                "OUTPUT":
+                output_dir + '/' + neutral + '/' + var,
+                "NEUTRAL":
+                neutral,
+                "DL":
+                delta_low,
+                "DH":
+                delta_high,
+                "BL":
+                bu_low,
+                "BH":
+                bu_high
+            }
+            make_shell_script(templatePath, scriptPath, substitutions)
+            # run_process(["sh", scriptPath])
+            run_process(["qsub", scriptPath])
