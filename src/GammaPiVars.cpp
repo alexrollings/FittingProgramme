d7b5c6b38d15efc5c88e8f406591dd2321598d9a
Alexandra Rollings <alexandra.rollings@cern.ch>
2019-12-19 09:24:08 +0000

    Added B->Dh back in

--- src/GammaPiVars.cpp
+++ src/GammaPiVars.cpp
@@ -307,13 +307,17 @@ NeutralBachelorVars<Neutral::gamma, Bachelor::pi>::NeutralBachelorVars(
                            .c_str(),
                        // "", 1.0404e+02, 50, 200),
                        // Fixed from data
-                       "", 1.3848e+02),
+                       // "", 1.3848e+02),
+                       // Fixed from data
+                       "", 1.3071e+02),
       MisRec_sigmaRBu_(("MisRec_sigmaRBu_" +
                         ComposeName(uniqueId, Neutral::gamma, Bachelor::pi))
                            .c_str(),
                        // "", 9.6206e+01, 50, 150),
                        // Fixed from data
-                       "", 8.3472e+01),
+                       // "", 8.3472e+01),
+                       // Fixed from data
+                       "", 8.4614e+01),
       pdfBu_MisRec_(("pdfBu_MisRec_" +
                      ComposeName(uniqueId, Neutral::gamma, Bachelor::pi))
                         .c_str(),
@@ -328,14 +332,18 @@ NeutralBachelorVars<Neutral::gamma, Bachelor::pi>::NeutralBachelorVars(
                                   .c_str(),
                               // "", 8.6958e+01, 50, 150),
       // Fixed from data
-      "", 1.0871e+02),
+      // "", 1.0871e+02),
+      // Fixed from data
+      "", 1.1831e+02),
       MisRec_sigmaRBuPartial_(("MisRec_sigmaRBuPartial_" +
                                ComposeName(uniqueId, Neutral::gamma,
                                            Bachelor::pi))
                                   .c_str(),
-                              // "", 6.1290e+01, 50, 150),
+                              // "", 6.1290e+01, 10, 150),
       // Fixed from data
-      "", 5.1338e+01),
+      // "", 5.1338e+01),
+      // Fixed from data
+      "", 4.6699e+01),
       pdfBuPartial_MisRec_(
           ("pdfBuPartial_MisRec_" +
            ComposeName(uniqueId, Neutral::gamma, Bachelor::pi))
