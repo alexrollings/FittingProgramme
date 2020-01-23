ad731cab8285c44228d8851955ede0dbe905ef9b
Alexandra Rollings <alexandra.rollings@cern.ch>
2020-01-23 09:33:31 +0000

    Improved script with dicts

--- src/generate_toy_hists.py
+++ src/generate_toy_hists.py
@@ -2,7 +2,7 @@ import os, sys
 os.environ[
     "ROOT_INCLUDE_PATH"] = os.pathsep + "/home/rollings/MeerkatHEPForge/Meerkat/inc/"
 
-from ROOT import gSystem, gStyle, TChain, TH2F, TH1F, TCanvas, TFile
+from ROOT import gSystem, gStyle, TChain, TTree, TH2F, TH1F, TCanvas, TFile
 
 gSystem.Load("/home/rollings/MeerkatHEPForge/Meerkat/lib/libMeerkat.so")
 
@@ -23,8 +23,7 @@ bu_mass = 'Bu_Delta_M'
 delta_mass = 'Delta_M'
 
 tree = TChain("BtoDstar0h3_h1h2pi0RTuple")
-years = ['2011']
-# years = ['2011', '2012', '2015', '2016', '2017', '2018']
+years = ['2011', '2012', '2015', '2016', '2017', '2018']
 polarities = ['Up', 'Down']
 for y in years:
   for p in polarities:
@@ -53,12 +52,24 @@ data_hist = TH2F("data", "Histogram storing data",
                                            int(bu_nbins), bu_low, bu_high,
                                            int(delta_nbins), delta_low, delta_high)
 
+tree_file = TFile('tree_file.root', 'recreate')
+new_tree = tree.CloneTree(0)
+new_tree.GetEntry(0)
+
 for i in range(0, tree.GetEntries()):
   tree.GetEntry(i)
   m_bu = getattr(tree, bu_mass)
   m_delta = getattr(tree, delta_mass)
+  bdt1 = getattr(tree, 'BDT1')
+  bdt2 = getattr(tree, 'BDT2')
+  fd_zsig = getattr(tree, 'D0_FD_ZSIG')
+  m_dh = getattr(tree, 'D0h_M')
+  m_pi0 = getattr(tree, 'Pi0_M')
   if m_bu > bu_low and m_bu < bu_high and m_delta > delta_low and m_delta < bu_high:
-    data_hist.Fill(m_bu, m_delta)
+    if bdt1 > 0.05 and bdt2 > 0.05 and m_dh > 4900 and m_dh < 5200 and fd_zsig > 2:
+      if m_pi0 < 165 and m_pi0 > 125:
+        data_hist.Fill(m_bu, m_delta)
+        new_tree.Fill()
 
 # Define 1D phase spaces for each of the two variables
 phsp_bu = OneDimPhaseSpace("phsp_bu", bu_low, bu_high)
@@ -69,7 +80,7 @@ phsp = CombinedPhaseSpace("phsp", phsp_bu, phsp_delta)
 
 kde = BinnedKernelDensity("KernelPDF",
                         phsp,                     # Phase space
-                        tree,                     # Input ntuple
+                        new_tree,                 # Input ntuple
                         bu_mass, delta_mass,      # Variables to use
                         int(bu_nbins), int(delta_nbins),    # Number of bins
                         bu_kwidth, delta_kwidth,  # Kernel widths
@@ -87,118 +98,90 @@ kernel_hist.Sumw2()
 # kernel_hist.Write()
 
 # Project to get 1D distributions; first bin = 0, last = -1
-kernel_hist_bu = kernel_hist.ProjectionX("kernel_bu", 0, -1)
-kernel_hist_delta = kernel_hist.ProjectionY("kernel_delta", 0, -1)
-data_hist_bu = data_hist.ProjectionX("data_bu", 0, -1)
-data_hist_delta = data_hist.ProjectionY("data_delta", 0, -1)
+hist_dict = {
+    'bu': {
+        'kernel': kernel_hist.ProjectionX("kernel_bu", 0, -1),
+        'data': data_hist.ProjectionX("data_bu", 0, -1)
+    },
+    'delta': {
+        'kernel': kernel_hist.ProjectionY("kernel_delta", 0, -1),
+        'data': data_hist.ProjectionY("data_delta", 0, -1)
+    }
+}
+
+label_dict = {
+    'bu':
+        "$m_{B} - m_{D^*} + m_{D^*}^{PDG} \left[MeV/c^2\\right]$",
+    'delta':
+        "$m_{D^*} - m_{D} - m_{\pi^{0}} + m_{\pi^{0}}^{PDG} \left[MeV/c^2\\right]$"
+}
+
+xaxis_dict = {
+    'bu': np.linspace(bu_low, bu_high, bu_nbins),
+    'delta': np.linspace(delta_low, delta_high, delta_nbins)
+}
+
+u_dict = {'bu': {'kernel': [], 'data': []}, 'delta': {'kernel': [], 'data': []}}
+norm_dict = {'bu': {'kernel': [], 'data': []}, 'delta': {'kernel': [], 'data': []}}
+
+limits_dict = {
+    'bu': {
+        'min': bu_low,
+        'max': bu_high,
+        'bins': int(bu_nbins)
+    },
+    'delta': {
+        'min': delta_low,
+        'max': delta_high,
+        'bins': int(delta_nbins)
+    }
+}
+
+for mass, d in u_dict.items():
+  for pdf, u in d.items():
+    for b in range(0, limits_dict[mass]['bins']):
+      u.append(
+          ufloat(hist_dict[mass][pdf].GetBinContent(b),
+                 hist_dict[mass][pdf].GetBinError(b)))
+    norm_dict[mass][pdf] = np.divide(
+        u,
+        hist_dict[mass][pdf].GetEntries() * np.ones(limits_dict[mass]['bins']))
+    fig = plt.plot()
+    plt.errorbar(xaxis_dict[mass],
+                 unumpy.nominal_values(u),
+                 yerr=unumpy.std_devs(u))
+    plt.xlim(limits_dict[mass]['min'], limits_dict[mass]['max'])
+    plt.ylim(bottom=0.0)
+    plt.xlabel(label_dict[mass])
+    plt.savefig('kde_plots/' + mass + '_' + pdf + '.png')
+    plt.clf()
+  fig = plt.plot()
+  plt.errorbar(xaxis_dict[mass],
+               unumpy.nominal_values(norm_dict[mass]['kernel']),
+               yerr=unumpy.std_devs(norm_dict[mass]['kernel']),
+               label='KDE')
+  plt.errorbar(xaxis_dict[mass],
+               unumpy.nominal_values(norm_dict[mass]['data']),
+               yerr=unumpy.std_devs(norm_dict[mass]['data']),
+               label='Data')
+  plt.xlim(limits_dict[mass]['min'], limits_dict[mass]['max'])
+  plt.ylim(bottom=0.0)
+  plt.xlabel(label_dict[mass])
+  plt.legend(loc='upper right')
+  plt.savefig('kde_plots/' + mass + '_comb.png')
+  plt.clf()
 
 gStyle.SetOptStat(0)
-
-bu_label = "$m_{B} - m_{D^*} + m_{D^*}^{PDG} \left[MeV/c^2\\right]$"
-delta_label = "$m_{D^*} - m_{D} - m_{\pi^{0}} + m_{\pi^{0}}^{PDG} \left[MeV/c^2\\right]$"
-
-bu_xaxis = np.linspace(bu_low, bu_high, bu_nbins)
-kernel_vec_bu = []
-data_vec_bu = []
-for b in range(0, int(bu_nbins)):
-  kernel_vec_bu.append(
-      ufloat(kernel_hist_bu.GetBinContent(b), kernel_hist_bu.GetBinError(b)))
-  data_vec_bu.append(
-      ufloat(data_hist_bu.GetBinContent(b), data_hist_bu.GetBinError(b)))
-
-kernel_vec_bu = np.divide(kernel_vec_bu,
-                          kernel_hist_bu.GetEntries() * np.ones(int(bu_nbins)))
-data_vec_bu = np.divide(data_vec_bu,
-                        data_hist_bu.GetEntries() * np.ones(int(bu_nbins)))
-
-fig = plt.plot()
-plt.errorbar(bu_xaxis, unumpy.nominal_values(kernel_vec_bu),
-             yerr=unumpy.std_devs(kernel_vec_bu),
-             label='KDE')
-plt.errorbar(bu_xaxis, unumpy.nominal_values(data_vec_bu),
-             yerr=unumpy.std_devs(data_vec_bu),
-             label='Data')
-plt.xlim(bu_low, bu_high)
-plt.ylim(bottom = 0.0)
-plt.xlabel(bu_label)
-plt.savefig('kde_plots/BuMass.png')
-plt.clf()
-
-delta_xaxis = np.linspace(delta_low, delta_high, delta_nbins)
-kernel_vec_delta = []
-data_vec_delta = []
-for b in range(0, int(delta_nbins)):
-  kernel_vec_delta.append(
-      ufloat(kernel_hist_delta.GetBinContent(b), kernel_hist_delta.GetBinError(b)))
-  data_vec_delta.append(
-      ufloat(data_hist_delta.GetBinContent(b), data_hist_delta.GetBinError(b)))
-
-kernel_vec_delta = np.divide(kernel_vec_delta,
-                          kernel_hist_delta.GetEntries() * np.ones(int(delta_nbins)))
-data_vec_delta = np.divide(data_vec_delta,
-                        data_hist_delta.GetEntries() * np.ones(int(delta_nbins)))
-
-fig = plt.plot()
-plt.errorbar(delta_xaxis, unumpy.nominal_values(kernel_vec_delta),
-             yerr=unumpy.std_devs(kernel_vec_delta),
-             label='KDE')
-plt.errorbar(delta_xaxis, unumpy.nominal_values(data_vec_delta),
-             yerr=unumpy.std_devs(data_vec_delta),
-             label='Data')
-plt.xlim(delta_low, delta_high)
-plt.ylim(bottom = 0.0)
-plt.xlabel(delta_label)
-plt.savefig('kde_plots/DeltaMass.png')
-plt.clf()
-
-fig = plt.plot()
-plt.hist(unumpy.nominal_values(kernel_vec_bu),
-         int(bu_nbins),
-         range=(bu_low, bu_high),
-         density=True,
-         histtype='step', linestyle='dashed',
-         label='KDE')
-plt.hist(unumpy.nominal_values(data_vec_bu),
-         int(bu_nbins),
-         range=(bu_low, bu_high),
-         density=True,
-         histtype='step', linestyle='dashed',
-         label='Data')
-plt.xlim(bu_low, bu_high)
-plt.ylim(bottom = 0.0)
-plt.xlabel(bu_label)
-plt.savefig('kde_plots/BuMassHist.png')
-plt.clf()
-
-# fig = plt.plot()
-# plt.hist(unumpy.nominal_values(kernel_vec_delta),
-#          int(delta_nbins),
-#          range=(delta_low, delta_high),
-#          density=True,
-#          histtype='step', linestyle='dashed',
-#          label='KDE')
-# plt.hist(unumpy.nominal_values(data_vec_delta),
-#          int(delta_nbins),
-#          range=(delta_low, delta_high),
-#          density=True,
-#          histtype='step', linestyle='dashed',
-#          label='Data')
-# plt.xlim(delta_low, delta_high)
-# plt.ylim(bottom = 0.0)
-# plt.xlabel(delta_label)
-# plt.savefig('kde_plots/DeltaMassHist.png')
-# plt.clf()
-
-# canvas = TCanvas("canvas", "Kernel canvas", 1000, 500)
-# canvas.Divide(2,1)
-# canvas.cd(1)
-# kernel_hist.SetTitle(" ")
-# kernel_hist.SetXTitle(bu_label)
-# kernel_hist.SetYTitle(delta_label)
-# kernel_hist.Draw("zcol")
-# canvas.cd(2)
-# data_hist.SetTitle(" ")
-# data_hist.SetXTitle(bu_label)
-# data_hist.SetYTitle(delta_label)
-# data_hist.Draw("zcol")
-# canvas.Print("kde_plots/2DPlots.png")
+canvas = TCanvas("canvas", "canvas", 1000, 500)
+canvas.Divide(2,1)
+canvas.cd(1)
+kernel_hist.SetTitle(" ")
+kernel_hist.SetXTitle(label_dict['bu'])
+kernel_hist.SetYTitle(label_dict['delta'])
+kernel_hist.Draw("zcol")
+canvas.cd(2)
+data_hist.SetTitle(" ")
+data_hist.SetXTitle(label_dict['bu'])
+data_hist.SetYTitle(label_dict['delta'])
+data_hist.Draw("zcol")
+canvas.Print("kde_plots/2DPlots.png")
