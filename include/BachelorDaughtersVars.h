810abd506545274d3ea27f25ff731eba19eb55f9
Alexandra Rollings <alexandra.rollings@cern.ch>
2020-02-05 09:53:23 +0000

    Relative BRs to FAV mode: moved into BDVars rather than just daughters SF in DVars

---
+++ include/BachelorDaughtersVars.h
@@ -0,0 +1,82 @@
+#pragma once
+#include "Configuration.h"
+#include "RooAbsReal.h"
+#include "RooFormulaVar.h"
+// Templated classes/functions mean that the compiler will automatically create
+// a copy
+// of the entire class for you for every permutation of template arguments it is
+// called
+// with. This is handy because often a lot of the code is identical except for a
+// few
+// details.
+// However, everything the compiler needs to do so must be provided in the .h
+// file,
+// because otherwise it won't actually know how to cater to external callers to
+// your
+// class/function (unless you explicitly specialize, see below).
+
+template <Bachelor bachelor, Daughters daughters>
+class BachelorDaughtersVars {
+  
+ public:
+  // Inline Constructor
+  BachelorDaughtersVars(int uniqueId_);
+  // Inline Destructor - can both be implemented externally too:
+  ~BachelorDaughtersVars() {}
+  // Class Human
+  // {
+  // public:
+  //  ~Human(); //Destructor declaration
+  // };
+  //:: is the scoping operator. Destructor definition/implementation
+  // Human::~Human();
+  // { // code }
+  // Invoked when a class is deleted or goes out of scope: good place to reset
+  // variables/release dynamically allocated memory
+
+  using This_t = BachelorDaughtersVars<bachelor, daughters>;
+
+  // Get() method of PDF now doesn't always return the same PDF, but the same
+  // PDF for the given ID
+  static This_t &Get(int uniqueId_) {
+    static std::map<int, std::shared_ptr<This_t>> singletons;
+    // An iterator to a map is a std::pair<key, value>, so we need to call
+    // i->second to get the value
+    auto it = singletons.find(uniqueId_);  // Check if uniqueId_ already exists
+    if (it == singletons.end()) {
+      // If it doesn't, create it as a new unique_ptr by calling emplace, which
+      // will forward the pointer to the constructor of std::unique_ptr
+      it = singletons.emplace(uniqueId_, std::make_shared<This_t>(uniqueId_))
+               .first;
+    }
+    return *it->second;
+  }
+
+  // If RooShit wasn't so shit we would pass a const reference
+  int uniqueId() { return uniqueId_; }
+  const double &daughtersSF() { return daughtersSF_; }
+
+private:
+  // When we DO need to specialize certain cases, we can still do that (see
+  // below)...
+
+  // Indicate if only used by one daughters
+
+  int uniqueId_;
+  // BR for decay relative to FAV mode (Kπ, π)
+  const double kBR_;
+};
+
+// Now we just need to define the constructors separately so the values are
+// different
+
+// ...by telling it exactly which specializations we are providing:
+template <> BachelorDaughtersVars<Bachelor::pi, Daughters::kpi>::BachelorDaughtersVars(int uniqueId);
+template <> BachelorDaughtersVars<Bachelor::pi, Daughters::kk>::BachelorDaughtersVars(int uniqueId);
+template <> BachelorDaughtersVars<Bachelor::pi, Daughters::pipi>::BachelorDaughtersVars(int uniqueId);
+template <> BachelorDaughtersVars<Bachelor::pi, Daughters::pik>::BachelorDaughtersVars(int uniqueId);
+template <> BachelorDaughtersVars<Bachelor::k, Daughters::kpi>::BachelorDaughtersVars(int uniqueId);
+template <> BachelorDaughtersVars<Bachelor::k, Daughters::kk>::BachelorDaughtersVars(int uniqueId);
+template <> BachelorDaughtersVars<Bachelor::k, Daughters::pipi>::BachelorDaughtersVars(int uniqueId);
+template <> BachelorDaughtersVars<Bachelor::k, Daughters::pik>::BachelorDaughtersVars(int uniqueId);
+// Then we can safely put these in the .cpp-file somewhere to be linked later.
---
+++ src/BachelorDaughtersVars.cpp
@@ -0,0 +1,37 @@
+#include "BachelorDaughtersVars.h"
+#include "Configuration.h"
+#include "GlobalVars.h"
+
+template <>
+BachelorDaughtersVars<Bachelor::pi, Daughters::kpi>::BachelorDaughtersVars(int uniqueId)
+    : kBR_(1.0) {}
+
+template <>
+BachelorDaughtersVars<Bachelor::pi, Daughters::kk>::BachelorDaughtersVars(int uniqueId)
+    : kBR_(0.00408/0.0395) {}
+
+template <>
+BachelorDaughtersVars<Bachelor::pi, Daughters::pipi>::BachelorDaughtersVars(int uniqueId)
+    : kBR_(0.001455/0.0395) {}
+
+template <>
+BachelorDaughtersVars<Bachelor::pi, Daughters::pik>::BachelorDaughtersVars(int uniqueId)
+    : kBR_(0.0001364/0.0395) {}
+
+template <>
+BachelorDaughtersVars<Bachelor::k, Daughters::kpi>::BachelorDaughtersVars(int uniqueId)
+    : kBR_(1.0*(0.000397/0.00490)) {}
+
+template <>
+BachelorDaughtersVars<Bachelor::k, Daughters::kk>::BachelorDaughtersVars(int uniqueId)
+    : kBR_((0.00408/0.0395)*(0.000397/0.00490)) {}
+
+template <>
+BachelorDaughtersVars<Bachelor::k, Daughters::pipi>::BachelorDaughtersVars(int uniqueId)
+    : kBR_((0.001455/0.0395)*(0.000397/0.00490)) {}
+
+// Including R ADS
+template <>
+BachelorDaughtersVars<Bachelor::k, Daughters::pik>::BachelorDaughtersVars(int uniqueId)
+    : kBR_(0.001) {}
+
