2eb70fe85085259a19b4e6cff121435411d2bdfa
Alexandra Rollings <alexandra.rollings@cern.ch>
2020-03-26 12:01:15 +0000

    Edited printing param list to be better

--- results_analysis/systematic_params.py
+++ results_analysis/systematic_params.py
@@ -1,7 +1,6 @@
 import re, os, sys, io
 import argparse
 
-
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
   parser.add_argument('-i',
@@ -12,7 +11,7 @@ if __name__ == '__main__':
   args = parser.parse_args()
   input_dir = args.input_dir
 
-  d = { 'Name' : None, 'Value': None, 'Range': None }
+  d = {}
   if os.path.isdir(input_dir):
     for f in os.listdir(input_dir):
       m = re.search('ParameterInfo((?:_[A-Za-z]+)+).csv', f)
@@ -20,11 +19,11 @@ if __name__ == '__main__':
         syst_labels = re.findall('([A-Za-z]+)', m.group(1))
         print(' '.join(syst_labels) + ':')
         for line in open(input_dir + '/' + f, 'r'):
-          d['Name'] = line.strip().split(',')[0]
-          d['Value'] = line.strip().split(',')[1]
-          d['Range'] = line.strip().split(',')[2]
+          name = line.strip().split(',')[0]
+          d[name] = {
+              'Value': line.strip().split(',')[1],
+              'Range': line.strip().split(',')[2]
+          }
 
-  print(d)
-
-
-
+  for par, val_range in d.items():
+    print('\t' + par + ':\t' + val_range['Value'] + '\t' + val_range['Range'])
