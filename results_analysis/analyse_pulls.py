8146797c223dd243580dc7eacf43a58920f79816
Alexandra Rollings <alexandra.rollings@cern.ch>
2020-05-04 12:51:31 +0100

    Plot scans for all observables saved

--- results_analysis/analyse_pulls.py
+++ results_analysis/analyse_pulls.py
@@ -1,5 +1,5 @@
 import math, re, os, sys, glob
-from ROOT import TFile, RooFitResult, RooGaussian, RooDataSet
+from ROOT import TFile, RooFitResult, RooGaussian, RooDataSet, TObject
 import root_numpy as r_np
 import numpy as np
 import matplotlib.pyplot as plt
@@ -9,6 +9,7 @@ from scipy import stats
 from uncertainties import unumpy, ufloat
 from uncertainties.umath import *
 import argparse
+from useful_functions import return_label
 
 if __name__ == '__main__':
   # May need to comment out bu delta_partial stuff (for older box scans)
@@ -24,7 +25,7 @@ if __name__ == '__main__':
       type=str,
       help=
       'Parameter to analyse pulls from (sig = N_Bu2Dst0h_D0pi0_pi0_pi_total/N_Bu2Dst0h_D0gamma_gamma_pi_total/N_Bu2Dst0h_D0pi0_gamma_pi_total)',
-      required=True)
+      required=False)
   parser.add_argument('-v',
                       '--var',
                       type=str,
@@ -94,15 +95,6 @@ if __name__ == '__main__':
 
   os.chdir(input_dir)
 
-  # Array to store parameter pull widths and error on width
-  par_pull_widths_arr = []
-  # Array to store mean parameter value and std dev
-  par_val_arr = []
-  # Array to store mean parameter error and std dev
-  par_err_arr = []
-  # Array to store mean total signal yield and error
-  signal_yield_arr = []
-
   if neutral == 'pi0':
     # mode = 'Bu2Dst0pi_D0pi0'
     sig_decay = 'Bu2Dst0h_D0pi0_pi0_pi_kpi'
@@ -115,157 +107,228 @@ if __name__ == '__main__':
 
   # Name of branches in tree (box and or effs)
   if neutral == 'pi0':
-    branch_names = ['orEff_Bu2Dst0pi_D0pi0', 'boxEff_Bu2Dst0pi_D0pi0']
+    branch_names = [
+        'orEff_Bu2Dst0pi_D0pi0', 'orEffErr_Bu2Dst0pi_D0pi0',
+        'boxEff_Bu2Dst0pi_D0pi0', 'boxEffErr_Bu2Dst0pi_D0pi0'
+    ]
   else:
     branch_names = [
-        'orEff_Bu2Dst0pi_D0gamma', 'boxEff_Bu2Dst0pi_D0gamma',
-        'boxPartialEff_Bu2Dst0pi_D0gamma', 'orEff_Bu2Dst0pi_D0pi0',
-        'boxEff_Bu2Dst0pi_D0pi0', 'boxPartialEff_Bu2Dst0pi_D0pi0'
+        'orEff_Bu2Dst0pi_D0pi0', 'orEffErr_Bu2Dst0pi_D0pi0',
+        'boxEff_Bu2Dst0pi_D0pi0', 'boxEffErr_Bu2Dst0pi_D0pi0',
+        'boxPartialEff_Bu2Dst0pi_D0pi0', 'boxPartialEffErr_Bu2Dst0pi_D0pi0'
     ]
-  # branch_names = ['orEffSignal', 'boxEffSignal']
-  # Array to store efficiencies in
-  box_eff = []
-  or_eff = []
 
-  i = 0
-  # Loop over files and extract result of interest
-  for f in file_list:
-    print(f)
-    tf = TFile(f)
-    # Ib result, params are stored in order mean [0], std dev [1]
-    result_par_pull_widths = tf.Get('Result_Pull_' + param)
-    result_par_val = tf.Get('Result_Val_' + param)
-    result_par_err = tf.Get('Result_Err_' + param)
-    # result_signal_yield = tf.Get('Result_Val_N_Bu2Dst0h_D0gamma_gamma_pi')
-    # result_signal_yield_err = tf.Get('Result_Err_N_Bu2Dst0h_D0gamma_gamma_pi')
-    result_signal_yield = tf.Get('Result_Val_N_tot_' + sig_decay)
-    result_signal_yield_err = tf.Get('Result_Err_N_tot_' +
-                                     sig_decay)
-    # Final values of fit to pulls
-    par_pull_widths = result_par_pull_widths.floatParsFinal()
-    par_val = result_par_val.floatParsFinal()
-    par_err = result_par_err.floatParsFinal()
-    signal_yield = result_signal_yield.floatParsFinal()
-    signal_yield_err = result_signal_yield_err.floatParsFinal()
-    # Save value and error of width for each box dimn
-    if i == 0:
-      initial_width = ufloat(par_pull_widths[1].getVal(),
-                             par_pull_widths[1].getError())
-    par_pull_widths_arr.append(
-        ufloat(par_pull_widths[1].getVal(), par_pull_widths[1].getError()))
-    par_val_arr.append(ufloat(par_val[0].getVal(), par_err[0].getVal()))
-    par_err_arr.append(ufloat(par_err[0].getVal(), par_err[1].getVal()))
-    signal_yield_arr.append(
-        ufloat(signal_yield[0].getVal(), signal_yield_err[0].getVal()))
+  observables = [
+      'N_tot_Bu2Dst0h_D0pi0_pi0_pi_kpi',
+      'N_tot_Bu2Dst0h_D0pi0_pi0_pi_kk',
+      'N_tot_Bu2Dst0h_D0pi0_pi0_pi_pipi',
+      'R_CP_Bu2Dst0h_D0pi0_Blind_pi0_kk',
+      'R_Dst0KDst0pi_Bu2Dst0h_D0pi0_pi0_kpi',
+      'A_Bu2Dst0h_D0pi0_Blind_pi0_pi_kpi',
+      'A_Bu2Dst0h_D0pi0_Blind_pi0_k_kpi',
+      'A_Bu2Dst0h_D0pi0_Blind_pi0_pi_kk',
+      'A_Bu2Dst0h_D0pi0_Blind_pi0_k_kk',
+      'R_ADS_Bu2Dst0h_D0pi0_Blind_pi0_k_pik_total',
+      'R_ADS_Bu2Dst0h_D0pi0_Blind_pi0_pi_pik_total'
+      'R_ADS_Bu2Dst0h_D0pi0_Blind_pi0_k_pik_minus',
+      'R_ADS_Bu2Dst0h_D0pi0_Blind_pi0_pi_pik_minus',
+      'R_ADS_Bu2Dst0h_D0pi0_Blind_pi0_k_pik_plus',
+      'R_ADS_Bu2Dst0h_D0pi0_Blind_pi0_pi_pik_plus',
+      'N_tot_Bu2Dst0h_D0pi0_gamma_pi_kpi',
+      'N_tot_Bu2Dst0h_D0pi0_gamma_pi_kk',
+      'N_tot_Bu2Dst0h_D0pi0_gamma_pi_pipi',
+      'R_CP_Bu2Dst0h_D0pi0_Blind_gamma_kk',
+      'R_Dst0KDst0pi_Bu2Dst0h_D0pi0_gamma_kpi',
+      'A_Bu2Dst0h_D0pi0_Blind_gamma_pi_kpi',
+      'A_Bu2Dst0h_D0pi0_Blind_gamma_k_kpi',
+      'A_Bu2Dst0h_D0pi0_Blind_gamma_pi_kk',
+      'A_Bu2Dst0h_D0pi0_Blind_gamma_k_kk',
+      'R_ADS_Bu2Dst0h_D0pi0_Blind_gamma_k_pik_total',
+      'R_ADS_Bu2Dst0h_D0pi0_Blind_gamma_pi_pik_total',
+      'R_ADS_Bu2Dst0h_D0pi0_Blind_gamma_k_pik_minus',
+      'R_ADS_Bu2Dst0h_D0pi0_Blind_gamma_pi_pik_minus'
+      'R_ADS_Bu2Dst0h_D0pi0_Blind_gamma_k_pik_plus',
+      'R_ADS_Bu2Dst0h_D0pi0_Blind_gamma_pi_pik_plus',
+      'N_tot_Bu2Dst0h_D0pi0_pi0_pi_kpi',
+      'N_tot_Bu2Dst0h_D0pi0_pi0_pi_kk',
+      'N_tot_Bu2Dst0h_D0pi0_pi0_pi_pipi',
+      'R_CP_Bu2Dst0h_D0pi0_Blind_pi0_kk',
+      'R_Dst0KDst0pi_Bu2Dst0h_D0pi0_pi0_kpi',
+      'A_Bu2Dst0h_D0pi0_Blind_pi0_pi_kpi',
+      'A_Bu2Dst0h_D0pi0_Blind_pi0_k_kpi',
+      'A_Bu2Dst0h_D0pi0_Blind_pi0_pi_kk',
+      'A_Bu2Dst0h_D0pi0_Blind_pi0_k_kk',
+      'R_ADS_Bu2Dst0h_D0pi0_Blind_pi0_k_pik_total',
+      'R_ADS_Bu2Dst0h_D0pi0_Blind_pi0_pi_pik_total',
+      'R_ADS_Bu2Dst0h_D0pi0_Blind_pi0_k_pik_minus',
+      'R_ADS_Bu2Dst0h_D0pi0_Blind_pi0_pi_pik_minus',
+      'R_ADS_Bu2Dst0h_D0pi0_Blind_pi0_k_pik_plus',
+      'R_ADS_Bu2Dst0h_D0pi0_Blind_pi0_pi_pik_plus'
+  ]
+  if param is not None:
+    observables = [param]
 
-    # 2D array with row element eff_tree[0] - orEff = first column, boxEff = second
-    eff_tree = r_np.root2array(f, 'tree', branch_names)
-    # Error on boxEff and orEff are binomial = 1/N(sqrt(Np(1-p))) = 1/N_mcsqrt(N_mc*eff(1-eff))
-    if neutral == 'partial':
-      or_eff.append(
-          ufloat(
-              eff_tree[0][0], eff_tree[0][1]))
-      box_eff.append(
-          ufloat(
-              eff_tree[0][4], eff_tree[0][5]))
-    else:
-      or_eff.append(
-          ufloat(
-              eff_tree[0][0], eff_tree[0][1]))
-      box_eff.append(
-          ufloat(
-              eff_tree[0][2], eff_tree[0][3]))
+  for obs in observables:
 
-    if box_fit != None:
-      m = re.search(
-          'Result_' + box_fit + '.root', f)
-      if m:
+    # Array to store parameter pull widths and error on width
+    par_pull_widths_arr = []
+    # Array to store mean parameter value and std dev
+    par_val_arr = []
+    # Array to store mean parameter error and std dev
+    par_err_arr = []
+    # Array to store mean total signal yield and error
+    signal_yield_arr = []
+
+    # Array to store efficiencies in
+    box_eff = []
+    or_eff = []
+
+    i = 0
+    # Loop over files and extract result of interest
+    for f in file_list:
+      # print(f)
+      tf = TFile(f)
+      # Ib result, params are stored in order mean [0], std dev [1]
+      result_par_pull_widths = tf.Get('Result_Pull_' + obs)
+      if isinstance(result_par_pull_widths, RooFitResult):
+        result_par_val = tf.Get('Result_Val_' + obs)
+        result_par_err = tf.Get('Result_Err_' + obs)
+        # result_signal_yield = tf.Get('Result_Val_N_Bu2Dst0h_D0gamma_gamma_pi')
+        # result_signal_yield_err = tf.Get('Result_Err_N_Bu2Dst0h_D0gamma_gamma_pi')
+        result_signal_yield = tf.Get('Result_Val_N_tot_' + sig_decay)
+        result_signal_yield_err = tf.Get('Result_Err_N_tot_' +
+                                         sig_decay)
+        # Final values of fit to pulls
+        par_pull_widths = result_par_pull_widths.floatParsFinal()
+        par_val = result_par_val.floatParsFinal()
+        par_err = result_par_err.floatParsFinal()
+        signal_yield = result_signal_yield.floatParsFinal()
+        signal_yield_err = result_signal_yield_err.floatParsFinal()
+        # Save value and error of width for each box dimn
+        if i == 0:
+          initial_width = ufloat(par_pull_widths[1].getVal(),
+                                 par_pull_widths[1].getError())
+        par_pull_widths_arr.append(
+            ufloat(par_pull_widths[1].getVal(), par_pull_widths[1].getError()))
+        par_val_arr.append(ufloat(par_val[0].getVal(), par_err[0].getVal()))
+        par_err_arr.append(ufloat(par_err[0].getVal(), par_err[1].getVal()))
+        signal_yield_arr.append(
+            ufloat(signal_yield[0].getVal(), signal_yield_err[0].getVal()))
+
+        # 2D array with row element eff_tree[0] - orEff = first column, boxEff = second
+        eff_tree = r_np.root2array(f, 'tree', branch_names)
+        # Error on boxEff and orEff are binomial = 1/N(sqrt(Np(1-p))) = 1/N_mcsqrt(N_mc*eff(1-eff))
         if neutral == 'partial':
-          fit_eff = eff_tree[0][5]
+          or_eff.append(
+              ufloat(
+                  eff_tree[0][0], eff_tree[0][1]))
+          box_eff.append(
+              ufloat(
+                  eff_tree[0][4], eff_tree[0][5]))
         else:
-          fit_eff = eff_tree[0][1]
+          or_eff.append(
+              ufloat(
+                  eff_tree[0][0], eff_tree[0][1]))
+          box_eff.append(
+              ufloat(
+                  eff_tree[0][2], eff_tree[0][3]))
 
-    i = i + 1
+        if box_fit != None:
+          m = re.search(
+              'Result_' + box_fit + '.root', f)
+          if m:
+            if neutral == 'partial':
+              fit_box_eff = eff_tree[0][4]
+            else:
+              fit_box_eff = eff_tree[0][2]
 
-  shared_yield = np.array(np.divide(
-      np.multiply(signal_yield_arr[0], box_eff), or_eff),
-                          dtype=object)
-  frac_shared_yield = np.divide(shared_yield, signal_yield_arr)
+        i = i + 1
+      else:
+        print(f'Skipping {obs} as not contained in {f}')
+        continue
 
-  # for i in range(0, len(file_list)):
-  #   print(file_list[i] + ' ' + str(box_eff[i]) + ' ' + str(or_eff[i]) + ' ' + str(frac_shared_yield[i]))
+    if len(par_pull_widths_arr) == 0:
+      sys.exit('No parameters found')
 
-  linear_err_fn = (np.multiply(
-      np.divide(np.ones(len(file_list)) * math.sqrt(2), signal_yield_arr),
-      shared_yield) + np.divide((signal_yield_arr - shared_yield),
-                                signal_yield_arr)) * initial_width
-  # sqrt from unumpy as can handle uncertainty types
-  quadratic_err_fn = unumpy.sqrt(
-      np.square(
-          np.multiply(
-              np.divide(
-                  np.ones(len(file_list)) *
-                  math.sqrt(2), signal_yield_arr), shared_yield)) + np.
-      square(np.divide((signal_yield_arr -
-                        shared_yield), signal_yield_arr))) * initial_width
+    shared_yield = np.array(np.divide(
+        np.multiply(signal_yield_arr[0], box_eff), or_eff),
+                            dtype=object)
+    frac_shared_yield = np.divide(shared_yield, signal_yield_arr)
 
-  perc_err_arr = np.divide(par_err_arr, par_val_arr)*100
+    # for i in range(0, len(file_list)):
+    #   print(file_list[i] + ' ' + str(box_eff[i]) + ' ' + str(or_eff[i]) + ' ' + str(frac_shared_yield[i]))
 
-  # par_pull_widths_arr = np.divide(par_pull_widths_arr, np.ones(len(file_list))*initial_width)
-  fig = plt.figure()
-  # plt.errorbar(
-  #     unumpy.nominal_values(frac_shared_yield),
-  #     unumpy.nominal_values(quadratic_err_fn),
-  #     xerr=unumpy.std_devs(frac_shared_yield),
-  #     yerr=unumpy.std_devs(quadratic_err_fn),
-  #     label=
-  #     '$\\frac{\sigma_{N_{T}}}{\sigma_{fit}}=\sqrt{(\\frac{N_{Box}\sqrt{2}}{N_{T}})^{2}+(\\frac{N_{T}-N_{Box}}{N_{T}})^{2}}$'
-  # )
-  # plt.errorbar(
-  #     unumpy.nominal_values(frac_shared_yield),
-  #     unumpy.nominal_values(linear_err_fn),
-  #     xerr=unumpy.std_devs(frac_shared_yield),
-  #     yerr=unumpy.std_devs(linear_err_fn),
-  #     label=
-  #     '$\\frac{\sigma_{N_{T}}}{\sigma_{fit}}=\\frac{N_{Box}\sqrt{2}}{N_{T}}+\\frac{N_{T}-N_{Box}}{N_{T}}$'
-  # )
-  plt.errorbar(unumpy.nominal_values(box_eff),
-               # unumpy.nominal_values(frac_shared_yield),
-               unumpy.nominal_values(par_pull_widths_arr),
-               xerr=unumpy.std_devs(box_eff),
-               # xerr=unumpy.std_devs(frac_shared_yield),
-               yerr=unumpy.std_devs(par_pull_widths_arr),
-               color='black', ecolor='lightgray',
-               label='Pseudo-experiments')
-  if box_fit != None:
-    plt.axvline(x=fit_eff, color='r', linestyle='dotted')
-  # plt.legend(loc='upper left')
-  plt.xlabel('$\epsilon_{Box}$')
-  # plt.xlabel('$N_{Box}/N_{T}$')
-  plt.ylim(1.0, 1.4)
-  plt.ylabel('Pull Width')
-  plt.title(param)
-  fig.savefig(
-      '/home/rollings/Bu2Dst0h_2d/FittingProgramme/results_analysis/box_scans/box_yield_vs_'
-      + param + '_pull_width_' + neutral + '_' + var + '.pdf')
-  # print(frac_shared_yield)
-  fig = plt.figure()
-  plt.errorbar(
-      unumpy.nominal_values(box_eff),
-      # unumpy.nominal_values(frac_shared_yield),
-      unumpy.nominal_values(perc_err_arr),
-      xerr=unumpy.std_devs(box_eff),
-      # xerr=unumpy.std_devs(frac_shared_yield),
-      yerr=unumpy.std_devs(perc_err_arr),
-      color='black',
-      ecolor='lightgray',
-      label='Pseudo-experiments')
-  # plt.xlabel('$N_{Box}/N_{T}$')
-  if box_fit != None:
-    plt.axvline(x=fit_eff, color='r', linestyle='dotted')
-  plt.xlabel('$\epsilon_{Box}$')
-  plt.ylabel('% Error')
-  plt.title(param)
-  fig.savefig(
-      '/home/rollings/Bu2Dst0h_2d/FittingProgramme/results_analysis/box_scans/box_yield_vs_'
-      + param + '_err_' + neutral + '_' + var + '.pdf')
+    linear_err_fn = (np.multiply(
+        np.divide(np.ones(len(file_list)) * math.sqrt(2), signal_yield_arr),
+        shared_yield) + np.divide((signal_yield_arr - shared_yield),
+                                  signal_yield_arr)) * initial_width
+    # sqrt from unumpy as can handle uncertainty types
+    quadratic_err_fn = unumpy.sqrt(
+        np.square(
+            np.multiply(
+                np.divide(
+                    np.ones(len(file_list)) *
+                    math.sqrt(2), signal_yield_arr), shared_yield)) + np.
+        square(np.divide((signal_yield_arr -
+                          shared_yield), signal_yield_arr))) * initial_width
+
+    perc_err_arr = np.divide(par_err_arr, par_val_arr)*100
+
+    # par_pull_widths_arr = np.divide(par_pull_widths_arr, np.ones(len(file_list))*initial_width)
+    fig = plt.figure()
+    # plt.errorbar(
+    #     unumpy.nominal_values(frac_shared_yield),
+    #     unumpy.nominal_values(quadratic_err_fn),
+    #     xerr=unumpy.std_devs(frac_shared_yield),
+    #     yerr=unumpy.std_devs(quadratic_err_fn),
+    #     label=
+    #     '$\\frac{\sigma_{N_{T}}}{\sigma_{fit}}=\sqrt{(\\frac{N_{Box}\sqrt{2}}{N_{T}})^{2}+(\\frac{N_{T}-N_{Box}}{N_{T}})^{2}}$'
+    # )
+    # plt.errorbar(
+    #     unumpy.nominal_values(frac_shared_yield),
+    #     unumpy.nominal_values(linear_err_fn),
+    #     xerr=unumpy.std_devs(frac_shared_yield),
+    #     yerr=unumpy.std_devs(linear_err_fn),
+    #     label=
+    #     '$\\frac{\sigma_{N_{T}}}{\sigma_{fit}}=\\frac{N_{Box}\sqrt{2}}{N_{T}}+\\frac{N_{T}-N_{Box}}{N_{T}}$'
+    # )
+    plt.errorbar(unumpy.nominal_values(box_eff),
+                 # unumpy.nominal_values(frac_shared_yield),
+                 unumpy.nominal_values(par_pull_widths_arr),
+                 xerr=unumpy.std_devs(box_eff),
+                 # xerr=unumpy.std_devs(frac_shared_yield),
+                 yerr=unumpy.std_devs(par_pull_widths_arr),
+                 color='black', ecolor='lightgray',
+                 label='Pseudo-experiments')
+    if box_fit != None:
+      plt.axvline(x=fit_box_eff, color='r', linestyle='dotted')
+    # plt.legend(loc='upper left')
+    plt.xlabel('$\epsilon_{Box}$')
+    # plt.xlabel('$N_{Box}/N_{T}$')
+    plt.ylim(1.0, 1.4)
+    plt.ylabel('Pull Width')
+    plt.title(return_label(obs))
+    fig.savefig(
+        '/home/rollings/Bu2Dst0h_2d/FittingProgramme/results_analysis/box_scans/box_yield_vs_'
+        + obs + '_pull_width_' + neutral + '_' + var + '.pdf')
+    # print(frac_shared_yield)
+    fig = plt.figure()
+    plt.errorbar(
+        unumpy.nominal_values(box_eff),
+        # unumpy.nominal_values(frac_shared_yield),
+        unumpy.nominal_values(perc_err_arr),
+        xerr=unumpy.std_devs(box_eff),
+        # xerr=unumpy.std_devs(frac_shared_yield),
+        yerr=unumpy.std_devs(perc_err_arr),
+        color='black',
+        ecolor='lightgray',
+        label='Pseudo-experiments')
+    # plt.xlabel('$N_{Box}/N_{T}$')
+    if box_fit != None:
+      plt.axvline(x=fit_box_eff, color='r', linestyle='dotted')
+    plt.xlabel('$\epsilon_{Box}$')
+    plt.ylabel('% Error')
+    plt.title(return_label(obs))
+    fig.savefig(
+        '/home/rollings/Bu2Dst0h_2d/FittingProgramme/results_analysis/box_scans/box_yield_vs_'
+        + obs + '_err_' + neutral + '_' + var + '.pdf')
