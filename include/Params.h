090c0ac1913414181ae9f3dcf7e8f27c95f778b7
Alexandra Rollings <alexandra.rollings@cern.ch>
2020-06-29 14:13:13 +0100

    Added CreateFloating for NDVars

--- include/Params.h
+++ include/Params.h
@@ -185,6 +185,18 @@ class Params {
                                       max_value);
   }
 
+  std::shared_ptr<RooRealVar> CreateFloating(std::string const &name,
+                                             int uniqueId, Neutral neutral,
+                                             Daughters daughters, double start,
+                                             double min_value,
+                                             double max_value) {
+    auto key = std::make_tuple(name, std::to_string(uniqueId),
+                               EnumToString(neutral), EnumToString(daughters), "");
+    auto var_name = name + "_" + ComposeName(uniqueId, neutral, daughters);
+    return ConstructFloatingParameter(key, var_name, start, min_value,
+                                      max_value);
+  }
+
   template <typename Iterator>
   void RandomiseParameters(Iterator const &systematic_begin,
                            Iterator const &systematic_end, TRandom3 &random) {
