7d78398c4f4a492be110ec98955864d4ad69e801
Alexandra Rollings <alexandra.rollings@cern.ch>
2020-06-29 13:21:00 +0100

    Edit unique ID in NBVars

--- include/NeutralBachelorVars.h
+++ include/NeutralBachelorVars.h
@@ -16,22 +16,22 @@
 template <Neutral neutral, Bachelor bachelor>
 class NeutralBachelorVars {
  public:
-  NeutralBachelorVars(int uniqueId_);
+  NeutralBachelorVars(int uniqueId);
   ~NeutralBachelorVars() {}
 
   using This_t = NeutralBachelorVars<neutral, bachelor>;
 
   // Get() method of PDF now doesn't always return the same PDF, but the same
   // PDF for the given ID
-  static This_t &Get(int uniqueId_) {
+  static This_t &Get(int uniqueId) {
     static std::map<int, std::shared_ptr<This_t>> singletons;
     // An iterator to a map is a std::pair<key, value>, so we need to call
     // i->second to get the value
-    auto it = singletons.find(uniqueId_);  // Check if uniqueId_ already exists
+    auto it = singletons.find(uniqueId);  // Check if uniqueId already exists
     if (it == singletons.end()) {
       // If it doesn't, create it as a new unique_ptr by calling emplace, which
       // will forward the pointer to the constructor of std::unique_ptr
-      it = singletons.emplace(uniqueId_, std::make_shared<This_t>(uniqueId_))
+      it = singletons.emplace(uniqueId, std::make_shared<This_t>(uniqueId))
                .first;
     }
     return *it->second;
