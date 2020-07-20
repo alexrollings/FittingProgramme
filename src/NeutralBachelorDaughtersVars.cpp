9e0e1584b375bb8078374655f10e6a77d3de45a3
Alexandra Rollings <alexandra.rollings@cern.ch>
2020-07-20 14:45:16 +0100

    Added gamma WN to pi kk instantiation in NDBVars

--- src/NeutralBachelorDaughtersVars.cpp
+++ src/NeutralBachelorDaughtersVars.cpp
@@ -370,6 +370,10 @@ NeutralBachelorDaughtersVarsImpl<_neutral, Bachelor::pi, Daughters::kk>::
           uniqueId, "A_Bu2Dst0h_D0pi0_",
           NeutralBachelorVars<_neutral, Bachelor::pi>::Get(uniqueId)
               .A_CP_Bu2Dst0h_D0pi0())),
+      A_Bu2Dst0h_D0gamma_WN_(Make_A_D2CP<_neutral, Bachelor::pi, Daughters::kk>(
+          uniqueId, "A_Bu2Dst0h_D0gamma_WN_",
+          NeutralBachelorVars<_neutral, Bachelor::pi>::Get(uniqueId)
+              .A_CP_Bu2Dst0h_D0gamma_WN())),
       A_Bu2Dst0h_D0pi0_WN_(Make_A_D2CP<_neutral, Bachelor::pi, Daughters::kk>(
           uniqueId, "A_Bu2Dst0h_D0pi0_WN_",
           NeutralBachelorVars<_neutral, Bachelor::pi>::Get(uniqueId)
@@ -391,6 +395,11 @@ NeutralBachelorDaughtersVarsImpl<_neutral, Bachelor::pi, Daughters::kk>::
            ComposeName(uniqueId, _neutral, Bachelor::pi, Daughters::kk))
               .c_str(),
           *A_Bu2Dst0h_D0pi0_)),
+      a_Bu2Dst0h_D0gamma_WN_(MakeLittleAsym(
+          ("a_Bu2Dst0h_D0gamma_WN_" +
+           ComposeName(uniqueId, _neutral, Bachelor::pi, Daughters::kk))
+              .c_str(),
+          *A_Bu2Dst0h_D0gamma_WN_)),
       a_Bu2Dst0h_D0pi0_WN_(MakeLittleAsym(
           ("a_Bu2Dst0h_D0pi0_WN_" +
            ComposeName(uniqueId, _neutral, Bachelor::pi, Daughters::kk))
@@ -424,6 +433,7 @@ NeutralBachelorDaughtersVarsImpl<_neutral, Bachelor::pi, Daughters::kk>::
               GlobalVars::Get(uniqueId).kBF_D02kk().getVal() /
               GlobalVars::Get(uniqueId).kBF_D02kpi().getVal(),
           -1000000, 1000000)),
+      N_tot_Bu2Dst0h_D0gamma_WN_(nullptr),
       N_tot_Bu2Dst0h_D0pi0_WN_(Make_N_WN<_neutral, Bachelor::pi, Daughters::kk>(
           uniqueId, "N_tot_Bu2Dst0h_D0pi0_WN_", *N_tot_Bu2Dst0h_D0pi0_,
           NeutralVars<_neutral>::Get(uniqueId).globalFrac_Bu2Dst0h_D0pi0_WN())),
@@ -452,14 +462,25 @@ NeutralBachelorDaughtersVarsImpl<_neutral, Bachelor::pi, Daughters::kk>::
           0, 1000000)),
       N_tot_Bs2Dst0Kpi_(nullptr),
       N_tot_Bs2D0Kpi_(nullptr) {
+  N_tot_Bu2Dst0h_D0gamma_ =
+      std::shared_ptr<RooRealVar>(Params::Get().CreateFloating(
+          "N_tot_Bu2Dst0h_D0gamma", uniqueId, _neutral, Bachelor::pi,
+          Daughters::kk,
+          NeutralBachelorDaughtersVars<_neutral, Bachelor::pi,
+                                       Daughters::kpi>::Get(uniqueId)
+                  .N_tot_Bu2Dst0h_D0gamma()
+                  .getVal() *
+              GlobalVars::Get(uniqueId).kBF_D02kk().getVal() /
+              GlobalVars::Get(uniqueId).kBF_D02kpi().getVal(),
+          -1000000, 1000000));
   if (_neutral == Neutral::gamma) {
-    N_tot_Bu2Dst0h_D0gamma_ =
+    N_tot_Bu2Dst0h_D0gamma_WN_ =
         std::shared_ptr<RooRealVar>(Params::Get().CreateFloating(
-            "N_tot_Bu2Dst0h_D0gamma", uniqueId, _neutral, Bachelor::pi,
+            "N_tot_Bu2Dst0h_D0gamma_WN", uniqueId, _neutral, Bachelor::pi,
             Daughters::kk,
             NeutralBachelorDaughtersVars<_neutral, Bachelor::pi,
                                          Daughters::kpi>::Get(uniqueId)
-                    .N_tot_Bu2Dst0h_D0gamma()
+                    .N_tot_Bu2Dst0h_D0gamma_WN()
                     .getVal() *
                 GlobalVars::Get(uniqueId).kBF_D02kk().getVal() /
                 GlobalVars::Get(uniqueId).kBF_D02kpi().getVal(),
@@ -474,6 +495,12 @@ NeutralBachelorDaughtersVarsImpl<_neutral, Bachelor::pi, Daughters::kk>::
          ComposeName(uniqueId, _neutral, Bachelor::pi, Daughters::kk))
             .c_str(),
         *A_Bu2Dst0h_D0gamma_));
+  } else {
+    N_tot_Bu2Dst0h_D0gamma_WN_ = std::shared_ptr<RooFormulaVar>(
+        Make_N_WN<_neutral, Bachelor::pi, Daughters::kk>(
+            uniqueId, "N_tot_Bu2Dst0h_D0gamma_WN_", *N_tot_Bu2Dst0h_D0gamma_,
+            NeutralVars<_neutral>::Get(uniqueId)
+                .globalFrac_Bu2Dst0h_D0gamma_WN()));
   }
 }
 
