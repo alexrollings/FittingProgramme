5580d58255bed4b3d8a32355901b0f5a9fd8099e
Alexandra Rollings <alexandra.rollings@cern.ch>
2020-07-22 09:00:29 +0100

    Added N_tot's to π πK in NBDVars

--- src/NeutralBachelorDaughtersVars.cpp
+++ src/NeutralBachelorDaughtersVars.cpp
@@ -375,6 +375,28 @@ NeutralBachelorDaughtersVarsImpl<_neutral, Bachelor::pi, Daughters::pik>::
               .N_tot_Bu2Dst0h_D0pi0_WN(),
           NeutralBachelorVars<_neutral, Bachelor::pi>::Get(uniqueId)
               .R_ADS_Bu2Dst0h_D0pi0_WN())),
+      N_tot_Bd2Dsth_(Make_N_tot_pik<_neutral, Bachelor::pi>(
+          uniqueId, "N_tot_Bd2Dsth_",
+          NeutralBachelorDaughtersVars<_neutral, Bachelor::pi,
+                                       Daughters::kpi>::Get(uniqueId)
+              .N_tot_Bd2Dsth(),
+          NeutralBachelorVars<_neutral, Bachelor::pi>::Get(uniqueId)
+              .R_ADS_Bd2Dsth())),
+      N_tot_Bu2D0hst_(Make_N_tot_pik<_neutral, Bachelor::pi>(
+          uniqueId, "N_tot_Bu2D0hst_",
+          NeutralBachelorDaughtersVars<_neutral, Bachelor::pi,
+                                       Daughters::kpi>::Get(uniqueId)
+              .N_tot_Bu2D0hst(),
+          NeutralBachelorVars<_neutral, Bachelor::pi>::Get(uniqueId)
+              .R_ADS_Bu2D0hst())),
+      N_tot_Bu2Dst0hst_D0pi0_(Make_N_tot_pik<_neutral, Bachelor::pi>(
+          uniqueId, "N_tot_Bu2Dst0hst_D0pi0_",
+          NeutralBachelorDaughtersVars<_neutral, Bachelor::pi,
+                                       Daughters::kpi>::Get(uniqueId)
+              .N_tot_Bu2Dst0hst_D0pi0(),
+          NeutralBachelorVars<_neutral, Bachelor::pi>::Get(uniqueId)
+              .R_ADS_Bu2Dst0hst_D0pi0())),
+      N_tot_Bu2Dst0hst_D0gamma_(nullptr),
       N_tot_Bu2Dst0h_D0gamma_FAVasSUP_(
           Make_N_tot_FAVasSUP<_neutral, Bachelor::pi>(
               uniqueId, "N_tot_Bu2Dst0h_D0gamma_FAVasSUP_",
@@ -479,6 +501,14 @@ NeutralBachelorDaughtersVarsImpl<_neutral, Bachelor::pi, Daughters::pik>::
                 .N_tot_Bu2Dst0h_D0gamma(),
             NeutralBachelorVars<_neutral, Bachelor::pi>::Get(uniqueId)
                 .R_ADS_Bu2Dst0h_D0gamma()));
+    N_tot_Bu2Dst0hst_D0gamma_ =
+        std::shared_ptr<RooFormulaVar>(Make_N_tot_pik<_neutral, Bachelor::pi>(
+            uniqueId, "N_tot_Bu2Dst0hst_D0gamma_",
+            NeutralBachelorDaughtersVars<_neutral, Bachelor::pi,
+                                         Daughters::kpi>::Get(uniqueId)
+                .N_tot_Bu2Dst0hst_D0gamma(),
+            NeutralBachelorVars<_neutral, Bachelor::pi>::Get(uniqueId)
+                .R_ADS_Bu2Dst0hst_D0gamma()));
   }
 }
 
