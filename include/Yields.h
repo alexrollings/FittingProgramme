26487a2290db890804086770034e2904fc2f03fb
Alexandra Rollings <alexandra.rollings@cern.ch>
2020-07-22 10:01:17 +0100

    Initialised N_misId_BuPartial in Yields

--- include/Yields.h
+++ include/Yields.h
@@ -1531,6 +1531,28 @@ void Yields<_neutral, _bachelor, _daughters, _charge>::AssignMisIdYields() {
             NeutralBachelorVars<_neutral, _bachelor>::Get(uniqueId_)
                 .buPartialEffMisId_Bu2Dst0h_D0pi0_WN()));
     if (_bachelor == Bachelor::k) {
+      N_misId_BuPartial_Bd2Dsth_ = std::unique_ptr<RooFormulaVar>(
+          Make_N_1D<_neutral, _bachelor, _daughters, _charge>(
+              uniqueId_, "N_misId_BuPartial_Bd2Dsth_", *N_misId_Bd2Dsth_,
+              NeutralBachelorVars<_neutral, _bachelor>::Get(uniqueId_)
+                  .buPartialEffMisId_Bd2Dsth()));
+      N_misId_BuPartial_Bu2D0hst_ = std::unique_ptr<RooFormulaVar>(
+          Make_N_1D<_neutral, _bachelor, _daughters, _charge>(
+              uniqueId_, "N_misId_BuPartial_Bu2D0hst_", *N_misId_Bu2D0hst_,
+              NeutralBachelorVars<_neutral, _bachelor>::Get(uniqueId_)
+                  .buPartialEffMisId_Bu2D0hst()));
+      N_misId_BuPartial_Bu2Dst0hst_D0pi0_ = std::unique_ptr<RooFormulaVar>(
+          Make_N_1D<_neutral, _bachelor, _daughters, _charge>(
+              uniqueId_, "N_misId_BuPartial_Bu2Dst0hst_D0pi0_",
+              *N_misId_Bu2Dst0hst_D0pi0_,
+              NeutralBachelorVars<_neutral, _bachelor>::Get(uniqueId_)
+                  .buPartialEffMisId_Bu2Dst0hst_D0pi0()));
+      N_misId_BuPartial_Bu2Dst0hst_D0gamma_ = std::unique_ptr<RooFormulaVar>(
+          Make_N_1D<_neutral, _bachelor, _daughters, _charge>(
+              uniqueId_, "N_misId_BuPartial_Bu2Dst0hst_D0gamma_",
+              *N_misId_Bu2Dst0hst_D0gamma_,
+              NeutralBachelorVars<_neutral, _bachelor>::Get(uniqueId_)
+                  .buPartialEffMisId_Bu2Dst0hst_D0gamma()));
       N_misId_BuPartial_MisRec_ = std::unique_ptr<RooFormulaVar>(
           Make_N_1D<_neutral, _bachelor, _daughters, _charge>(
               uniqueId_, "N_misId_BuPartial_MisRec_", *N_misId_MisRec_,
