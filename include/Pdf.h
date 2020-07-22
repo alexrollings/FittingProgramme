d1feb2ecdfae2bf29964c8d9688728c9a39a2c27
Alexandra Rollings <alexandra.rollings@cern.ch>
2020-07-22 11:05:00 +0100

    Bu2Dst0hst_D0pi0 added

--- include/Pdf.h
+++ include/Pdf.h
@@ -695,12 +695,12 @@ void Pdf<_neutral, _bachelor, _daughters, _charge>::CreateBuAddPdf() {
   PdfBase::yieldsBu_.add(
       Yields<_neutral, _bachelor, _daughters, _charge>::Get(PdfBase::uniqueId_)
           .N_trueId_Bu_Bu2D0hst());
-  // PdfBase::functionsBu_.add(
-  //     NeutralBachelorVars<_neutral, _bachelor>::Get(PdfBase::uniqueId_)
-  //         .pdfBu_Bu2Dst0hst_D0pi0());
-  // PdfBase::yieldsBu_.add(
-  //     Yields<_neutral, _bachelor, _daughters, _charge>::Get(PdfBase::uniqueId_)
-  //         .N_trueId_Bu_Bu2Dst0hst_D0pi0());
+  PdfBase::functionsBu_.add(
+      NeutralBachelorVars<_neutral, _bachelor>::Get(PdfBase::uniqueId_)
+          .pdfBu_Bu2Dst0hst_D0pi0());
+  PdfBase::yieldsBu_.add(
+      Yields<_neutral, _bachelor, _daughters, _charge>::Get(PdfBase::uniqueId_)
+          .N_trueId_Bu_Bu2Dst0hst_D0pi0());
   // if (_neutral == Neutral::gamma) {
   //   PdfBase::functionsBu_.add(
   //       NeutralBachelorVars<_neutral, _bachelor>::Get(PdfBase::uniqueId_)
@@ -880,12 +880,12 @@ void Pdf<_neutral, _bachelor, _daughters, _charge>::CreateBuPartialAddPdf() {
   PdfBase::yieldsBuPartial_.add(
       Yields<_neutral, _bachelor, _daughters, _charge>::Get(PdfBase::uniqueId_)
           .N_trueId_BuPartial_Bu2D0hst());
-  // PdfBase::functionsBuPartial_.add(
-  //     NeutralBachelorVars<_neutral, _bachelor>::Get(PdfBase::uniqueId_)
-  //         .pdfBuPartial_Bu2Dst0hst_D0pi0());
-  // PdfBase::yieldsBuPartial_.add(
-  //     Yields<_neutral, _bachelor, _daughters, _charge>::Get(PdfBase::uniqueId_)
-  //         .N_trueId_BuPartial_Bu2Dst0hst_D0pi0());
+  PdfBase::functionsBuPartial_.add(
+      NeutralBachelorVars<_neutral, _bachelor>::Get(PdfBase::uniqueId_)
+          .pdfBuPartial_Bu2Dst0hst_D0pi0());
+  PdfBase::yieldsBuPartial_.add(
+      Yields<_neutral, _bachelor, _daughters, _charge>::Get(PdfBase::uniqueId_)
+          .N_trueId_BuPartial_Bu2Dst0hst_D0pi0());
   // PdfBase::functionsBuPartial_.add(
   //     NeutralBachelorVars<_neutral, _bachelor>::Get(PdfBase::uniqueId_)
   //         .pdfBuPartial_Bu2Dst0hst_D0gamma());
