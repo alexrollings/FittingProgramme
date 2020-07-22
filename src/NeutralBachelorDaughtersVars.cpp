9d2aae7162dc1659669ac04eb2870b32054fc1d7
Alexandra Rollings <alexandra.rollings@cern.ch>
2020-07-22 09:09:35 +0100

    Added a's to π ππ in NBDVars

--- src/NeutralBachelorDaughtersVars.cpp
+++ src/NeutralBachelorDaughtersVars.cpp
@@ -790,6 +790,22 @@ NeutralBachelorDaughtersVarsImpl<_neutral, Bachelor::pi, Daughters::pipi>::
            ComposeName(uniqueId, _neutral, Bachelor::pi, Daughters::pipi))
               .c_str(),
           *A_Bu2Dst0h_D0pi0_WN_)),
+      a_Bd2Dsth_(MakeLittleAsym(
+          ("a_Bd2Dsth_" +
+           ComposeName(uniqueId, _neutral, Bachelor::pi, Daughters::pipi))
+              .c_str(),
+          *A_Bd2Dsth_)),
+      a_Bu2D0hst_(MakeLittleAsym(
+          ("a_Bu2D0hst_" +
+           ComposeName(uniqueId, _neutral, Bachelor::pi, Daughters::pipi))
+              .c_str(),
+          *A_Bu2D0hst_)),
+      a_Bu2Dst0hst_D0pi0_(MakeLittleAsym(
+          ("a_Bu2Dst0hst_D0pi0_" +
+           ComposeName(uniqueId, _neutral, Bachelor::pi, Daughters::pipi))
+              .c_str(),
+          *A_Bu2Dst0hst_D0pi0_)),
+      a_Bu2Dst0hst_D0gamma_(nullptr),
       a_MisRec_(MakeLittleAsym(
           ("a_MisRec_" +
            ComposeName(uniqueId, _neutral, Bachelor::pi, Daughters::pipi))
@@ -882,6 +898,11 @@ NeutralBachelorDaughtersVarsImpl<_neutral, Bachelor::pi, Daughters::pipi>::
          ComposeName(uniqueId, _neutral, Bachelor::pi, Daughters::pipi))
             .c_str(),
         *A_Bu2Dst0h_D0gamma_));
+    a_Bu2Dst0hst_D0gamma_ = std::unique_ptr<RooFormulaVar>(MakeLittleAsym(
+        ("a_Bu2Dst0hst_D0gamma_" +
+         ComposeName(uniqueId, _neutral, Bachelor::pi, Daughters::pipi))
+            .c_str(),
+        *A_Bu2Dst0hst_D0gamma_));
   } else {
     N_tot_Bu2Dst0h_D0gamma_WN_ =
         std::shared_ptr<RooRealVar>(Params::Get().CreateFloating(
