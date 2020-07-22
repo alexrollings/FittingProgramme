708aa8b1402b35154f85a267b66d0064427fc074
Alexandra Rollings <alexandra.rollings@cern.ch>
2020-07-22 09:02:36 +0100

    Added A's to π KK in NBDVars

--- src/NeutralBachelorDaughtersVars.cpp
+++ src/NeutralBachelorDaughtersVars.cpp
@@ -528,6 +528,19 @@ NeutralBachelorDaughtersVarsImpl<_neutral, Bachelor::pi, Daughters::kk>::
           uniqueId, "A_Bu2Dst0h_D0pi0_WN_",
           NeutralBachelorVars<_neutral, Bachelor::pi>::Get(uniqueId)
               .A_CP_Bu2Dst0h_D0pi0_WN())),
+      A_Bd2Dsth_(Make_A_D2CP<_neutral, Bachelor::pi, Daughters::kk>(
+          uniqueId, "A_Bd2Dsth_",
+          NeutralBachelorVars<_neutral, Bachelor::pi>::Get(uniqueId)
+              .A_CP_Bd2Dsth())),
+      A_Bu2D0hst_(Make_A_D2CP<_neutral, Bachelor::pi, Daughters::kk>(
+          uniqueId, "A_Bu2D0hst_",
+          NeutralBachelorVars<_neutral, Bachelor::pi>::Get(uniqueId)
+              .A_CP_Bu2D0hst())),
+      A_Bu2Dst0hst_D0pi0_(Make_A_D2CP<_neutral, Bachelor::pi, Daughters::kk>(
+          uniqueId, "A_Bu2Dst0hst_D0pi0_",
+          NeutralBachelorVars<_neutral, Bachelor::pi>::Get(uniqueId)
+              .A_CP_Bu2Dst0hst_D0pi0())),
+      A_Bu2Dst0hst_D0gamma_(nullptr),
       A_MisRec_(Params::Get().CreateFloating("A_MisRec", uniqueId, _neutral,
                                              Bachelor::pi, Daughters::kk, 0, -5,
                                              5)),
@@ -634,6 +647,11 @@ NeutralBachelorDaughtersVarsImpl<_neutral, Bachelor::pi, Daughters::kk>::
             uniqueId, "A_Bu2Dst0h_D0gamma_",
             NeutralBachelorVars<_neutral, Bachelor::pi>::Get(uniqueId)
                 .A_CP_Bu2Dst0h_D0gamma()));
+    A_Bu2Dst0hst_D0gamma_ = std::shared_ptr<RooAbsReal>(
+        Make_A_D2CP<_neutral, Bachelor::pi, Daughters::kk>(
+            uniqueId, "A_Bu2Dst0hst_D0gamma_",
+            NeutralBachelorVars<_neutral, Bachelor::pi>::Get(uniqueId)
+                .A_CP_Bu2Dst0hst_D0gamma()));
     a_Bu2Dst0h_D0gamma_ = std::unique_ptr<RooFormulaVar>(MakeLittleAsym(
         ("a_Bu2Dst0h_D0gamma_" +
          ComposeName(uniqueId, _neutral, Bachelor::pi, Daughters::kk))
