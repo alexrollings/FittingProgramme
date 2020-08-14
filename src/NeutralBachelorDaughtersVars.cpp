9ff52b091b031b6513a38d954614d596131c51ce
Alexandra Rollings <alexandra.rollings@cern.ch>
2020-08-14 11:06:18 +0100

    Fix systematic on background for A FAV D*π to 0.5%, due to small CP violation available in this mode

--- src/NeutralBachelorDaughtersVars.cpp
+++ src/NeutralBachelorDaughtersVars.cpp
@@ -123,15 +123,15 @@ NeutralBachelorDaughtersVarsImpl<_neutral, Bachelor::pi, Daughters::kpi>::
       A_Bd2Dsth_(Params::Get().CreateFixed("A_Bd2Dsth", uniqueId, _neutral,
                                            Bachelor::pi, Daughters::kpi, 0, 0.0,
                                            Systematic::NA, Sign::none)),
-      A_Bu2D0hst_(Params::Get().CreateFixed("A_Bu2D0hst", uniqueId, _neutral,
-                                            Bachelor::pi, Daughters::kpi, 0,
-                                            0.02, Systematic::NA, Sign::none)),
+      A_Bu2D0hst_(Params::Get().CreateFixed(
+          "A_Bu2D0hst", uniqueId, _neutral, Bachelor::pi, Daughters::kpi, 0,
+          0.0005, Systematic::NA, Sign::none)),
       A_Bu2Dst0hst_D0pi0_(Params::Get().CreateFixed(
           "A_Bu2Dst0hst_D0pi0", uniqueId, _neutral, Bachelor::pi,
-          Daughters::kpi, 0, 0.02, Systematic::NA, Sign::none)),
+          Daughters::kpi, 0, 0.0005, Systematic::NA, Sign::none)),
       A_Bu2Dst0hst_D0gamma_(Params::Get().CreateFixed(
           "A_Bu2Dst0hst_D0gamma", uniqueId, _neutral, Bachelor::pi,
-          Daughters::kpi, 0, 0.02, Systematic::NA, Sign::none)),
+          Daughters::kpi, 0, 0.0005, Systematic::NA, Sign::none)),
       A_Bs2Dst0Kpi_(nullptr),
       A_Bs2D0Kpi_(nullptr),
       a_Bu2Dst0h_D0gamma_(MakeLittleAsym(
