a96d97f7c9b5184e40db524475f93fc394dc0eda
Alexandra Rollings <alexandra.rollings@cern.ch>
2020-10-16 10:11:32 +0100

    Increased max of bkgFracADSBs2Dst0Kpi to 20

--- src/Pi0Vars.cpp
+++ src/Pi0Vars.cpp
@@ -744,7 +744,7 @@ NeutralVars<Neutral::pi0>::NeutralVars(int uniqueId)
       bkgFracKK_Lb2Omegach_Lcpi0_(Params::Get().CreateFloating(
           "bkgFracKK_Lb2Omegach_Lcpi0", uniqueId_, Neutral::pi0, 0.1, 0, 5)),
       bkgFracADS_Bs2Dst0Kpi_(Params::Get().CreateFloating(
-          "bkgFracADS_Bs2Dst0Kpi", uniqueId_, Neutral::pi0, 4, -2, 15)),
+          "bkgFracADS_Bs2Dst0Kpi", uniqueId_, Neutral::pi0, 4, -2, 20)),
       // bkgFracADS_Bs2D0Kpi_(Params::Get().CreateFloating(
       //     "bkgFracADS_Bs2D0Kpi", uniqueId_, Neutral::pi0, 4, -2, 10)),
       bkgFracADS_Bs2D0Kpi_(Params::Get().CreateFixed(
