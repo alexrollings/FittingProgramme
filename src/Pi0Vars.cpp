36c01d575e2b5740a734255799ff14087bee1480
Alexandra Rollings <alexandra.rollings@cern.ch>
2020-10-19 12:14:34 +0100

    Lowred bkgFracADS_Bs2Dst0Kpi_ max and star at 5 rther than 6 in Pi0Vars

--- src/Pi0Vars.cpp
+++ src/Pi0Vars.cpp
@@ -792,7 +792,7 @@ NeutralVars<Neutral::pi0>::NeutralVars(int uniqueId)
       bkgFracKK_Lb2Omegach_Lcpi0_(Params::Get().CreateFloating(
           "bkgFracKK_Lb2Omegach_Lcpi0", uniqueId_, Neutral::pi0, 0.6, 0, 5)),
       bkgFracADS_Bs2Dst0Kpi_(Params::Get().CreateFloating(
-          "bkgFracADS_Bs2Dst0Kpi", uniqueId_, Neutral::pi0, 6, -2, 20)),
+          "bkgFracADS_Bs2Dst0Kpi", uniqueId_, Neutral::pi0, 5, -2, 10)),
       // bkgFracADS_Bs2D0Kpi_(Params::Get().CreateFloating(
       //     "bkgFracADS_Bs2D0Kpi", uniqueId_, Neutral::pi0, 4, -2, 10)),
       bkgFracADS_Bs2D0Kpi_(Params::Get().CreateFixed(
