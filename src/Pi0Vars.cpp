561868841ad4cd799fabe7b9647329b63396b831
Alexandra Rollings <alexandra.rollings@cern.ch>
2020-10-30 13:23:16 +0000

    Initialise Bd to data fit final result in Pi0V

--- src/Pi0Vars.cpp
+++ src/Pi0Vars.cpp
@@ -227,11 +227,11 @@ NeutralVars<Neutral::pi0>::NeutralVars(int uniqueId)
       Bu2Dst0h_D0gamma_WN_meanBuPartial_(nullptr),
       Bu2Dst0h_D0gamma_WN_KpiSigmaBuPartial_(nullptr),
       // -------------------- Bu2Dst0h_WN -------------------- //
-      Bu2Dst0h_WN_floatingFracD0pi0_(Params::Get().CreateFloating(
-          "Bu2Dst0h_WN_floatingFracD0pi0", uniqueId_, Neutral::pi0, 1, 0, 5)),
-      // Bu2Dst0h_WN_floatingFracD0pi0_(Params::Get().CreateFixed(
-      //     "Bu2Dst0h_WN_floatingFracD0pi0", uniqueId_, Neutral::pi0, 1, 0.,
-      //     Systematic::Bu2Dst0h_WN_Frac, Sign::same)),
+      // Bu2Dst0h_WN_floatingFracD0pi0_(Params::Get().CreateFloating(
+      //     "Bu2Dst0h_WN_floatingFracD0pi0", uniqueId_, Neutral::pi0, 1, -1, 2)),
+      Bu2Dst0h_WN_floatingFracD0pi0_(Params::Get().CreateFixed(
+          "Bu2Dst0h_WN_floatingFracD0pi0", uniqueId_, Neutral::pi0, 1, 0.,
+          Systematic::NA, Sign::same)),
       // -------------------- Bd2Dsth -------------------- //
       Bd2Dsth_thresholdDelta_(Params::Get().CreateFixed(
           "Bd2Dsth_thresholdDelta", uniqueId_, Neutral::pi0, Mode::Bd2Dstpi,
@@ -795,15 +795,14 @@ NeutralVars<Neutral::pi0>::NeutralVars(int uniqueId)
           "bkgFracKK_Lb2Omegach_Lcpi0", uniqueId_, Neutral::pi0, 0.1, 0, 5)),
       bkgFracADS_Bs2Dst0Kpi_(Params::Get().CreateFloating(
           "bkgFracADS_Bs2Dst0Kpi", uniqueId_, Neutral::pi0, 4, -2, 15)),
-      // bkgFracADS_Bs2D0Kpi_(Params::Get().CreateFixed(
-      //     "bkgFracADS_Bs2D0Kpi", uniqueId_, Neutral::pi0,
-      //     ReadBkgFracs<Neutral::pi0>(Mode::Bs2D0Kpi, "val"),
-      //     ReadBkgFracs<Neutral::pi0>(Mode::Bs2D0Kpi, "std"),
-      //     Systematic::Bs2D0Kpi_BkgFrac, Sign::same)) {}
-      //     "bkgFracADS_Bs2D0Kpi", uniqueId_, Neutral::pi0, 4, -2, 10)),
-      bkgFracADS_Bs2D0Kpi_(Params::Get().CreateFloating(
+      bkgFracADS_Bs2D0Kpi_(Params::Get().CreateFixed(
           "bkgFracADS_Bs2D0Kpi", uniqueId_, Neutral::pi0,
-          ReadBkgFracs<Neutral::pi0>(Mode::Bs2D0Kpi, "val"), 0, 5)) {}
+          ReadBkgFracs<Neutral::pi0>(Mode::Bs2D0Kpi, "val"),
+          ReadBkgFracs<Neutral::pi0>(Mode::Bs2D0Kpi, "std"),
+          Systematic::Bs2D0Kpi_BkgFrac, Sign::same)) {}
+      // bkgFracADS_Bs2D0Kpi_(Params::Get().CreateFloating(
+      //     "bkgFracADS_Bs2D0Kpi", uniqueId_, Neutral::pi0,
+      //     ReadBkgFracs<Neutral::pi0>(Mode::Bs2D0Kpi, "val"), 0, 5)) {}
 // constraint_bkgFracGlobal_WN_(("constraint_bkgFracGlobal_WN_" +
 //                               ComposeName(uniqueId_, Neutral::pi0))
 //                                  .c_str(),
