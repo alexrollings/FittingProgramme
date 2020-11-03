9413a2313489801e9e5912ee64d7f85e9ab48363
Alexandra Rollings <alexandra.rollings@cern.ch>
2020-11-03 13:22:01 +0000

    Updated results_analysis/systematics_to_json.py to fix bug: resaving dict to file and memory leak from root

--- results_analysis/systematics_to_json.py
+++ results_analysis/systematics_to_json.py
@@ -5,6 +5,12 @@ import argparse
 import json
 from shutil import copyfile
 
+def SaveToFile(fname, json_dict):
+  with open(fname, 'w') as json_file:
+    json.dump(json_dict, json_file)
+  # Make back up file before overwriting again
+  copyfile(fname, fname + '.bak')
+
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
   parser.add_argument(
@@ -37,9 +43,8 @@ if __name__ == '__main__':
     json_fname = f'{input_dir}/systematics_{neutral}.json'
     # If json file exists, load existing dict
     if os.path.exists(json_fname):
-      with open(json_fname) as json_file:
+      with open(json_fname, 'r') as json_file:
         json_dict = json.load(json_file)
-      json_file.close()
     # If not, create empty dict
     else:
         json_dict = {}
@@ -47,40 +52,43 @@ if __name__ == '__main__':
     sys.exit(input_dir + ' does not exist.')
 
   input_dir = input_dir + '/results/'
-  with open(json_fname, 'w') as json_file:
-    if os.path.isdir(input_dir):
-      count = 0
-      for f in os.listdir(input_dir):
-        m = re.search(
-            'SystResult_' + box_str + '((?:_[0-9A-Za-z]+)+)_([a-z0-9]+).root', f)
-            # 'SystResult(?:_[0-9]+){4,6}((?:_[0-9A-Za-z]+)+)_[a-z0-9]+.root', f)
-        if m:
-          syst_label = m.group(1)[1:]
-          seed = m.group(2)
-          if syst_label not in json_dict:
-            json_dict[syst_label] = {}
-          if seed not in json_dict[syst_label]:
-            count = count + 1
-            syst_file = TFile(os.path.join(input_dir, f))
-            syst_result = syst_file.Get('SystResult')
-            # Label each result with syst label and seed
-            # Save covariance matrix quality and fit status
-            json_dict[syst_label][seed] = {'covQual' : syst_result.covQual(), 'fitStatus' : syst_result.status()}
-            # Save obs names and values
-            for p in syst_result.floatParsFinal():
-              for obs in observables:
-                par_name = p.GetName()[:-2]
-                m = re.match(obs + '(_[A-Za-z][0-9])+', par_name)
-                if m:
-                  json_dict[syst_label][seed][par_name] = p.getVal()
-            # Dump to json every 100 files
-            if count == 100:
-              print('Saving to json')
-              json.dump(json_dict, json_file)
-              # Make back up file before overwriting again
-              copyfile(json_fname, json_fname + '.bak')
-              count = 0
-    else:
-      sys.exit(input_dir + ' does not exist.')
+  if os.path.isdir(input_dir):
+    count = 0
+    files = os.listdir(input_dir)
+    n_files = len(files)
+    for f in files:
+      m = re.search(
+          f'SystResult_{box_str}((?:_[0-9A-Za-z]+)+)_([a-z0-9]+).root', f)
+          # 'SystResult(?:_[0-9]+){4,6}((?:_[0-9A-Za-z]+)+)_[a-z0-9]+.root', f)
+      if m:
+        syst_label = m.group(1)[1:]
+        seed = m.group(2)
+        if syst_label not in json_dict:
+          json_dict[syst_label] = {}
+        if seed not in json_dict[syst_label]:
+          count = count + 1
+          syst_file = TFile(os.path.join(input_dir, f))
+          syst_result = syst_file.Get('SystResult')
+          # Label each result with syst label and seed
+          # Save covariance matrix quality and fit status
+          json_dict[syst_label][seed] = {'covQual' : syst_result.covQual(), 'fitStatus' : syst_result.status()}
+          # Save obs names and values
+          for p in syst_result.floatParsFinal():
+            for obs in observables:
+              par_name = p.GetName()[:-2]
+              m = re.match(obs + '(_[A-Za-z][0-9])+', par_name)
+              if m:
+                json_dict[syst_label][seed][par_name] = p.getVal()
+          syst_file.Close()
+          # Dump to json every 100 files
+          if count % 100 == 0:
+            SaveToFile(json_fname, json_dict)
+            print(f'Saved result {count} out of {n_files}')
+            # if count == 1000:
+            #   sys.exit()
+    print(f'Saved result final {count} out of {n_files}')
+    SaveToFile(json_fname, json_dict)
+  else:
+    sys.exit(input_dir + ' does not exist.')
 
 
