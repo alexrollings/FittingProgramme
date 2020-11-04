a9b2a934b819af64c6f87c684c206deb74c34157
Alexandra Rollings <alexandra.rollings@cern.ch>
2020-11-04 17:06:05 +0000

    Commend out A_CP_Lb systs in config.h

--- include/Configuration.h
+++ include/Configuration.h
@@ -160,9 +160,8 @@ enum class Systematic {
   kBF_D02kk,
   kBF_D02pipi,
   // DON'T RUN BELOW
-  // π0
-  A_CP_pi_Lb2Omegach_Lcpi0,
-  A_CP_K_Lb2Omegach_Lcpi0,
+  // A_CP_pi_Lb2Omegach_Lcpi0,
+  // A_CP_K_Lb2Omegach_Lcpi0,
   Bs2D0Kpi_BkgFrac,
   kBF_Bu2Dst0pi,
   kBF_Bu2Dst0K,
