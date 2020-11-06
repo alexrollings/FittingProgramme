d88413af98981d7dcfb3657c70ecc7493ecba9a7
Alexandra Rollings <alexandra.rollings@cern.ch>
2020-11-06 07:12:35 +0000

    Check whether RooFitResult is in file before creating entry in dict in systematics_to_json.py

--- results_analysis/systematics_to_json.py
+++ results_analysis/systematics_to_json.py
@@ -68,28 +68,31 @@ if __name__ == '__main__':
         if syst_label not in json_dict:
           json_dict[syst_label] = {}
         if seed not in json_dict[syst_label]:
-          save_count = save_count + 1
           syst_file = TFile(os.path.join(input_dir, f))
           syst_result = syst_file.Get('SystResult')
-          # Label each result with syst label and seed
-          # Save covariance matrix quality and fit status
-          json_dict[syst_label][seed] = {'covQual' : syst_result.covQual(), 'fitStatus' : syst_result.status()}
-          # Save obs names and values
-          for p in syst_result.floatParsFinal():
-            for obs in observables:
-              par_name = p.GetName()
-              m = re.match(obs + '(\S+)_[0-9]+', par_name)
-              if m:
-                # Results labelled with different numbers
-                par_name = obs + m.group(1)
-                json_dict[syst_label][seed][par_name] = p.getVal()
-          syst_file.Close()
-          # Dump to json every 100 files
-          if save_count % 100 == 0:
-            SaveToFile(json_fname, json_dict)
-            print(f'Saved result {file_count} out of {n_files}')
-            # if save_count == 1000:
-            #   sys.exit()
+          if isinstance(syst_result, RooFitResult):
+            save_count = save_count + 1
+            # Label each result with syst label and seed
+            # Save covariance matrix quality and fit status
+            json_dict[syst_label][seed] = {'covQual' : syst_result.covQual(), 'fitStatus' : syst_result.status()}
+            # Save obs names and values
+            for p in syst_result.floatParsFinal():
+              for obs in observables:
+                par_name = p.GetName()
+                m = re.match(obs + '(\S+)_[0-9]+', par_name)
+                if m:
+                  # Results labelled with different numbers
+                  par_name = obs + m.group(1)
+                  json_dict[syst_label][seed][par_name] = p.getVal()
+            syst_file.Close()
+            # Dump to json every 100 files
+            if save_count % 100 == 0:
+              SaveToFile(json_fname, json_dict)
+              print(f'Saved result {file_count} out of {n_files}')
+              # if save_count == 1000:
+              #   sys.exit()
+          else:
+            print(f'SystResult not found in {f}')
     print(f'Saved result final {file_count} out of {n_files}')
     SaveToFile(json_fname, json_dict)
   else:
