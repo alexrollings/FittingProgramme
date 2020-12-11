06ac7122c880d2a789ba091a425ff0926654e601
Alexandra Rollings <alexandra.rollings@cern.ch>
2020-12-11 10:22:07 +0000

    Recalculate bkg fracs for Bs in calc_fixed_params/calc_bkgFacs.py

--- calc_fixed_params/calc_bkgFacs.py
+++ calc_fixed_params/calc_bkgFacs.py
@@ -71,6 +71,9 @@ if __name__ == '__main__':
   kBF_Bd2Dstrho = ufloat(6.8e-03, 0.9e-03)
   kBF_Dst02D0pi0 = ufloat(64.7e-02, 0.9e-02)
   kBF_Dst02D0gamma = ufloat(35.3e-02, 0.9e-02)
+  kBF_Bs2D0Kst0 = ufloat(4.4e-04, 0.6e-04)
+  kBF_D02kpi = ufloat(3.950e-02, 0.031e-02)
+  kBF_D02pik = ufloat(1.50e-04, 0.07e-04)
   kBR_Bs = ufloat(1.0, 0.25)
 
   frac_dict = {}
@@ -80,11 +83,21 @@ if __name__ == '__main__':
         (kBF_Bd2Dstpi * kBF_Dst2D0pi) / (kBF_Bu2Dst0pi * kBF_Dst02D0pi0)) * (
             mc_effs_dict['Bd2Dstpi'] / mc_effs_dict['Bu2Dst0pi_D0pi0']) * (
                 or_effs_dict['Bd2Dstpi'] / or_effs_dict['Bu2Dst0pi_D0pi0'])
+    frac_dict['Bs2D0Kst0'] = (
+        (kBF_Bs2D0Kst0 * kBF_D02kpi) /
+        (kBF_Bu2Dst0pi * kBF_Dst02D0pi0 * kBF_D02pik)) * (
+            mc_effs_dict['Bs2D0Kst0'] / mc_effs_dict['Bu2Dst0pi_D0pi0']) * (
+                or_effs_dict['Bs2D0Kst0'] / or_effs_dict['Bu2Dst0pi_D0pi0'])
   else:
     frac_dict['Bd2Dstpi'] = (
         (kBF_Bd2Dstpi * kBF_Dst2D0pi) / (kBF_Bu2Dst0pi * kBF_Dst02D0gamma)) * (
             mc_effs_dict['Bd2Dstpi'] / mc_effs_dict['Bu2Dst0pi_D0gamma']) * (
                 or_effs_dict['Bd2Dstpi'] / or_effs_dict['Bu2Dst0pi_D0gamma'])
+    frac_dict['Bs2D0Kst0'] = (
+        (kBF_Bs2D0Kst0 * kBF_D02kpi) /
+        (kBF_Bu2Dst0pi * kBF_Dst02D0gamma * kBF_D02pik)) * (
+            mc_effs_dict['Bs2D0Kst0'] / mc_effs_dict['Bu2Dst0pi_D0gamma']) * (
+                or_effs_dict['Bs2D0Kst0'] / or_effs_dict['Bu2Dst0pi_D0gamma'])
 
   frac_dict['Bu2D0rho'] = (
       (kBF_Bu2D0rho + kBF_Bd2D0rho0) / (kBF_Bd2Dstpi * kBF_Dst2D0pi)) * (
@@ -108,12 +121,12 @@ if __name__ == '__main__':
        mc_effs_dict['Bd2Dstpi']) *
       (or_effs_dict['Bu2Dst0rho_D0pi0'] / or_effs_dict['Bd2Dstpi']))
 
-  frac_dict['Bs2D0Kst0'] = kBR_Bs * (
-      (mc_effs_dict['Bs2D0Kst0'] * or_effs_dict['Bs2D0Kst0']) /
-      (kBF_Dst02D0pi0 * mc_effs_dict['Bs2Dst0Kst0_D0pi0'] *
-       or_effs_dict['Bs2Dst0Kst0_D0pi0'] +
-       kBF_Dst02D0gamma * mc_effs_dict['Bs2Dst0Kst0_D0gamma'] *
-       or_effs_dict['Bs2Dst0Kst0_D0gamma']))
+  frac_dict['Bs2Dst0Kst0'] = kBR_Bs * (
+      kBF_Dst02D0pi0 * mc_effs_dict['Bs2Dst0Kst0_D0pi0'] *
+      or_effs_dict['Bs2Dst0Kst0_D0pi0'] + kBF_Dst02D0gamma *
+      mc_effs_dict['Bs2Dst0Kst0_D0gamma'] * or_effs_dict['Bs2Dst0Kst0_D0gamma']
+  ) / (mc_effs_dict['Bs2D0Kst0'] * or_effs_dict['Bs2D0Kst0'])
+
 
   f_out = open(f'/home/rollings/Bu2Dst0h_2d/FittingProgramme/calc_fixed_params/bkgFracs_{neutral}.txt', 'w+')
   for mode, frac in frac_dict.items():
