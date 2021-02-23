dca86e990b50be97865deb3100e75a27e93c3e81
Alexandra Rollings <alexandra.rollings@cern.ch>
2021-02-23 10:59:36 +0000

    Script to calculated Bu2Dst0h gamma physica observables

---
+++ calc_fixed_params/gamma_obs.py
@@ -0,0 +1,165 @@
+import math
+from uncertainties import ufloat
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+phi3_av_err = (4.6 + 5.3)*0.5
+phi3_val_err = ufloat(math.radians(71.1),math.radians(phi3_av_err))
+phi3 = np.random.normal(phi3_val_err.n, phi3_val_err.s, 10000)
+
+# FAV: D0 -> K-π+
+rD_val = np.sqrt(0.00344)
+rD_err = rD_val * 2 * (2e-5/0.00344)
+rD_val_err = ufloat(rD_val,rD_err)
+rD = np.random.normal(rD_val_err.n, rD_val_err.s, 10000)
+
+dD_av_err = (8.6 + 10.2)*0.5
+dD_val_err = ufloat(math.radians(192.1),math.radians(dD_av_err))
+dD = np.random.normal(dD_val_err.n, dD_val_err.s, 10000)
+
+# B- -> D*0 π-
+rB_Dpi = np.random.uniform(0.0, 0.02, 10000)
+dB_Dpi = np.random.uniform(math.radians(0.0), math.radians(180.0), 10000)
+
+# B- -> D*0 K-
+rB_DK = np.random.uniform(0.0, 0.2, 10000)
+dB_DK_av_err = (18 + 10)*0.5
+dB_DK_val_err = ufloat(math.radians(314.0),math.radians(dB_DK_av_err))
+dB_DK = np.random.normal(dB_DK_val_err.n, dB_DK_val_err.s, 10000)
+
+BF_B2Dstpi = np.random.normal(4.90e-03, 0.17e-03, 10000)
+BF_B2DstK_av_err = (0.31e-04 + 0.28e-04)*0.5
+BF_B2DstK = np.random.normal(3.97e-04, BF_B2DstK_av_err, 10000)
+
+R_DstKDstpi = BF_B2DstK / BF_B2Dstpi
+
+pi0_Dpi_kpi_plus = 1 + rD**2 * rB_Dpi**2 + 2 * rD * rB_Dpi * np.cos(dB_Dpi - dD + phi3)
+pi0_Dpi_kpi_minus = 1 + rD**2 * rB_Dpi**2 + 2 * rD * rB_Dpi * np.cos(dB_Dpi - dD - phi3)
+gamma_Dpi_kpi_plus = 1 + rD**2 * rB_Dpi**2 - 2 * rD * rB_Dpi * np.cos(dB_Dpi - dD + phi3)
+gamma_Dpi_kpi_minus = 1 + rD**2 * rB_Dpi**2 - 2 * rD * rB_Dpi * np.cos(dB_Dpi - dD - phi3)
+
+pi0_Dpi_hh_plus = 1 + rB_Dpi**2 + 2 * rB_Dpi * np.cos(dB_Dpi + phi3)
+pi0_Dpi_hh_minus = 1 + rB_Dpi**2 + 2 * rB_Dpi * np.cos(dB_Dpi - phi3)
+gamma_Dpi_hh_plus = 1 + rB_Dpi**2 - 2 * rB_Dpi * np.cos(dB_Dpi + phi3)
+gamma_Dpi_hh_minus = 1 + rB_Dpi**2 - 2 * rB_Dpi * np.cos(dB_Dpi - phi3)
+
+pi0_Dpi_pik_plus = rB_Dpi**2 + rD**2 + 2 * rB_Dpi * rD * np.cos(dB_Dpi + dD + phi3)
+pi0_Dpi_pik_minus = rB_Dpi**2 + rD**2 + 2 * rB_Dpi * rD * np.cos(dB_Dpi + dD - phi3)
+gamma_Dpi_pik_plus = rB_Dpi**2 + rD**2 - 2 * rB_Dpi * rD * np.cos(dB_Dpi + dD + phi3)
+gamma_Dpi_pik_minus = rB_Dpi**2 + rD**2 - 2 * rB_Dpi * rD * np.cos(dB_Dpi + dD - phi3)
+
+pi0_DK_kpi_plus = 1 + rD**2 * rB_DK**2 + 2 * rD * rB_DK * np.cos(dB_DK - dD + phi3)
+pi0_DK_kpi_minus = 1 + rD**2 * rB_DK**2 + 2 * rD * rB_DK * np.cos(dB_DK - dD - phi3)
+gamma_DK_kpi_plus = 1 + rD**2 * rB_DK**2 - 2 * rD * rB_DK * np.cos(dB_DK - dD + phi3)
+gamma_DK_kpi_minus = 1 + rD**2 * rB_DK**2 - 2 * rD * rB_DK * np.cos(dB_DK - dD - phi3)
+
+pi0_DK_hh_plus = 1 + rB_DK**2 + 2 * rB_DK * np.cos(dB_DK + phi3)
+pi0_DK_hh_minus = 1 + rB_DK**2 + 2 * rB_DK * np.cos(dB_DK - phi3)
+gamma_DK_hh_plus = 1 + rB_DK**2 - 2 * rB_DK * np.cos(dB_DK + phi3)
+gamma_DK_hh_minus = 1 + rB_DK**2 - 2 * rB_DK * np.cos(dB_DK - phi3)
+
+pi0_DK_pik_plus = rB_DK**2 + rD**2 + 2 * rB_DK * rD * np.cos(dB_DK + dD + phi3)
+pi0_DK_pik_minus = rB_DK**2 + rD**2 + 2 * rB_DK * rD * np.cos(dB_DK + dD - phi3)
+gamma_DK_pik_plus = rB_DK**2 + rD**2 - 2 * rB_DK * rD * np.cos(dB_DK + dD + phi3)
+gamma_DK_pik_minus = rB_DK**2 + rD**2 - 2 * rB_DK * rD * np.cos(dB_DK + dD - phi3)
+
+A_pi0_Dpi_kpi = (pi0_Dpi_kpi_minus - pi0_Dpi_kpi_plus)/(pi0_Dpi_kpi_minus + pi0_Dpi_kpi_plus)
+A_gamma_Dpi_kpi = (gamma_Dpi_kpi_minus - gamma_Dpi_kpi_plus)/(gamma_Dpi_kpi_minus + gamma_Dpi_kpi_plus)
+
+A_pi0_DK_kpi = (pi0_DK_kpi_minus - pi0_DK_kpi_plus)/(pi0_DK_kpi_minus + pi0_DK_kpi_plus)
+A_gamma_DK_kpi = (gamma_DK_kpi_minus - gamma_DK_kpi_plus)/(gamma_DK_kpi_minus + gamma_DK_kpi_plus)
+
+A_pi0_Dpi_CP = (pi0_Dpi_hh_minus - pi0_Dpi_hh_plus)/(pi0_Dpi_hh_minus + pi0_Dpi_hh_plus)
+A_gamma_Dpi_CP = (gamma_Dpi_hh_minus - gamma_Dpi_hh_plus)/(gamma_Dpi_hh_minus + gamma_Dpi_hh_plus)
+
+A_pi0_DK_CP = (pi0_DK_hh_minus - pi0_DK_hh_plus)/(pi0_DK_hh_minus + pi0_DK_hh_plus)
+A_gamma_DK_CP = (gamma_DK_hh_minus - gamma_DK_hh_plus)/(gamma_DK_hh_minus + gamma_DK_hh_plus)
+
+R_pi0_CP = (pi0_DK_hh_minus + pi0_DK_hh_plus)/((pi0_Dpi_hh_minus + pi0_Dpi_hh_plus)*R_DstKDstpi)
+R_gamma_CP = (gamma_DK_hh_minus + gamma_DK_hh_plus)/((gamma_Dpi_hh_minus + gamma_Dpi_hh_plus)*R_DstKDstpi)
+
+R_pi0_Dpi_pik_minus = pi0_Dpi_pik_minus / pi0_Dpi_kpi_minus
+R_pi0_Dpi_pik_plus = pi0_Dpi_pik_plus / pi0_Dpi_kpi_plus
+R_gamma_Dpi_pik_minus = gamma_Dpi_pik_minus / gamma_Dpi_kpi_minus
+R_gamma_Dpi_pik_plus = gamma_Dpi_pik_plus / gamma_Dpi_kpi_plus
+
+R_pi0_Dpi_pik = (R_pi0_Dpi_pik_minus + R_pi0_Dpi_pik_plus) / 2
+A_pi0_Dpi_pik = (R_pi0_Dpi_pik_minus - R_pi0_Dpi_pik_plus) / (R_pi0_Dpi_pik_minus + R_pi0_Dpi_pik_plus)
+R_gamma_Dpi_pik = (R_gamma_Dpi_pik_minus + R_gamma_Dpi_pik_plus) / 2
+A_gamma_Dpi_pik = (R_gamma_Dpi_pik_minus - R_gamma_Dpi_pik_plus) / (R_gamma_Dpi_pik_minus + R_gamma_Dpi_pik_plus)
+
+R_pi0_DK_pik_minus = pi0_DK_pik_minus / pi0_DK_kpi_minus
+R_pi0_DK_pik_plus = pi0_DK_pik_plus / pi0_DK_kpi_plus
+R_gamma_DK_pik_minus = gamma_DK_pik_minus / gamma_DK_kpi_minus
+R_gamma_DK_pik_plus = gamma_DK_pik_plus / gamma_DK_kpi_plus
+
+R_pi0_DK_pik = (R_pi0_DK_pik_minus + R_pi0_DK_pik_plus) / 2
+A_pi0_DK_pik = (R_pi0_DK_pik_minus - R_pi0_DK_pik_plus) / (R_pi0_DK_pik_minus + R_pi0_DK_pik_plus)
+R_gamma_DK_pik = (R_gamma_DK_pik_minus + R_gamma_DK_pik_plus) / 2
+A_gamma_DK_pik = (R_gamma_DK_pik_minus - R_gamma_DK_pik_plus) / (R_gamma_DK_pik_minus + R_gamma_DK_pik_plus)
+
+fig = plt.hist(phi3, 100, density=True)
+plt.savefig('gamma_obs/phi3.png')
+plt.clf()
+fig = plt.hist(rD, 100, density=True)
+plt.savefig('gamma_obs/rD.png')
+plt.clf()
+fig = plt.hist(dD, 100, density=True)
+plt.savefig('gamma_obs/dD.png')
+plt.clf()
+fig = plt.hist(rB_Dpi, 100, density=True)
+plt.savefig('gamma_obs/rB_Dpi.png')
+plt.clf()
+fig = plt.hist(dB_Dpi, 100, density=True)
+plt.savefig('gamma_obs/dB_Dpi.png')
+plt.clf()
+fig = plt.hist(rB_DK, 100, density=True)
+plt.savefig('gamma_obs/rB_DK.png')
+plt.clf()
+fig = plt.hist(dB_DK, 100, density=True)
+plt.savefig('gamma_obs/dB_DK.png')
+plt.clf()
+fig = plt.hist(R_DstKDstpi, 100, density=True)
+plt.savefig('gamma_obs/R_DstKDstpi.png')
+plt.clf()
+fig = plt.hist(A_gamma_Dpi_kpi, 100, density=True)
+plt.savefig('gamma_obs/A_gamma_Dpi_kpi.png')
+plt.clf()
+fig = plt.hist(A_gamma_DK_kpi, 100, density=True)
+plt.savefig('gamma_obs/A_gamma_DK_kpi.png')
+plt.clf()
+fig = plt.hist(A_gamma_Dpi_CP, 100, density=True)
+plt.savefig('gamma_obs/A_gamma_Dpi_CP.png')
+plt.clf()
+fig = plt.hist(A_gamma_DK_CP, 100, density=True)
+plt.savefig('gamma_obs/A_gamma_DK_CP.png')
+plt.clf()
+fig = plt.hist(R_gamma_CP, 100, density=True)
+plt.savefig('gamma_obs/R_gamma_CP.png')
+plt.clf()
+fig = plt.hist(R_gamma_Dpi_pik_minus, 100, density=True)
+plt.savefig('gamma_obs/R_gamma_Dpi_pik_minus.png')
+plt.clf()
+fig = plt.hist(R_gamma_Dpi_pik_plus, 100, density=True)
+plt.savefig('gamma_obs/R_gamma_Dpi_pik_plus.png')
+plt.clf()
+fig = plt.hist(R_gamma_Dpi_pik, 100, density=True)
+plt.savefig('gamma_obs/R_gamma_Dpi_pik.png')
+plt.clf()
+fig = plt.hist(A_gamma_Dpi_pik, 100, density=True)
+plt.savefig('gamma_obs/A_gamma_Dpi_pik.png')
+plt.clf()
+fig = plt.hist(R_gamma_DK_pik_minus, 100, density=True)
+plt.savefig('gamma_obs/R_gamma_DK_pik_minus.png')
+plt.clf()
+fig = plt.hist(R_gamma_DK_pik_plus, 100, density=True)
+plt.savefig('gamma_obs/R_gamma_DK_pik_plus.png')
+plt.clf()
+fig = plt.hist(R_gamma_DK_pik, 100, density=True)
+plt.savefig('gamma_obs/R_gamma_DK_pik.png')
+plt.clf()
+fig = plt.hist(A_gamma_DK_pik, 100, density=True)
+plt.savefig('gamma_obs/A_gamma_DK_pik.png')
+plt.clf()
