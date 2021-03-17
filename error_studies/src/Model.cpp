92640616f66c374ed7a35415c32ed93cb1205362
Alexandra Rollings <alexandra.rollings@cern.ch>
2021-03-17 14:05:37 +0000

    Params for box 136_158_5230_5300

--- error_studies/src/Model.cpp
+++ error_studies/src/Model.cpp
@@ -2,27 +2,30 @@
 
 Model::Model(Configuration &config, int _uniqueId)
     : uniqueId(_uniqueId),
-      // buMean(("buMean_" + std::to_string(uniqueId)).c_str(), "", 5.2819e+03,
-      //        5250, 5290),
-      // buSigma(("buSigma_" + std::to_string(uniqueId)).c_str(), "", 20, 10,
-      // 50), buA1(("buA1_" + std::to_string(uniqueId)).c_str(), "", 1, 0, 5),
-      // buN1(("buN1_" + std::to_string(uniqueId)).c_str(), "", 10),
-      // buA2(("buA2_" + std::to_string(uniqueId)).c_str(), "", -1, -5,
-      // -0.00001), buN2(("buN2_" + std::to_string(uniqueId)).c_str(), "", 10),
-      // buFrac(("buFrac_" + std::to_string(uniqueId)).c_str(), "", 2.0967e-01,
-      // 0,
-      //        1),
-      // m(B): 5170 - 5370
-      buMean(("buMean_" + std::to_string(uniqueId)).c_str(), "", 5.2728e+03),
-      buSigma(("buSigma_" + std::to_string(uniqueId)).c_str(), "", 2.2420e+01),
-      buA1(("buA1_" + std::to_string(uniqueId)).c_str(), "", 4.3526e+00),
+      buMean(("buMean_" + std::to_string(uniqueId)).c_str(), "", 5.2819e+03,
+             5250, 5290),
+      buSigma(("buSigma_" + std::to_string(uniqueId)).c_str(), "", 20, 10, 50),
+      // buA1(("buA1_" + std::to_string(uniqueId)).c_str(), "", 1, 0, 5),
       buN1(("buN1_" + std::to_string(uniqueId)).c_str(), "", 10),
-      buA2(("buA2_" + std::to_string(uniqueId)).c_str(), "", -1.6871e+00),
+      // buA2(("buA2_" + std::to_string(uniqueId)).c_str(), "", -1, -5,
+      // -0.00001),
       buN2(("buN2_" + std::to_string(uniqueId)).c_str(), "", 10),
-      buFrac(("buFrac_" + std::to_string(uniqueId)).c_str(), "", 7.6254e-01),
-      // m(B): 5170 - 5220
+      buFrac(("buFrac_" + std::to_string(uniqueId)).c_str(), "", 2.0967e-01, 0,
+             1),
+      // m(B): 5230 - 5300
+      buA1(("buA1_" + std::to_string(uniqueId)).c_str(), "", 9.8052e-01),
+      buA2(("buA2_" + std::to_string(uniqueId)).c_str(), "", -2.2181e+00),
+      // m(B): 5170 - 5370
       // buMean(("buMean_" + std::to_string(uniqueId)).c_str(), "", 5.2728e+03),
       // buSigma(("buSigma_" + std::to_string(uniqueId)).c_str(),
+      // "", 2.2420e+01), buA1(("buA1_" + std::to_string(uniqueId)).c_str(),
+      // "", 4.3526e+00), buN1(("buN1_" + std::to_string(uniqueId)).c_str(), "",
+      // 10), buA2(("buA2_" + std::to_string(uniqueId)).c_str(), "",
+      // -1.6871e+00), buN2(("buN2_" + std::to_string(uniqueId)).c_str(), "",
+      // 10), buFrac(("buFrac_" + std::to_string(uniqueId)).c_str(),
+      // "", 7.6254e-01), m(B): 5170 - 5220 buMean(("buMean_" +
+      // std::to_string(uniqueId)).c_str(), "", 5.2728e+03), buSigma(("buSigma_"
+      // + std::to_string(uniqueId)).c_str(),
       // "", 2.2356e+01), buA1(("buA1_" + std::to_string(uniqueId)).c_str(),
       // "", 4.3526e+00), buN1(("buN1_" + std::to_string(uniqueId)).c_str(), "",
       // 10), buA2(("buA2_" + std::to_string(uniqueId)).c_str(), "",
@@ -35,30 +38,32 @@ Model::Model(Configuration &config, int _uniqueId)
              buMean, buSigma, buA2, buN2),
       buPdf(("buPdf_" + std::to_string(uniqueId)).c_str(), "",
             RooArgSet(buPdf1, buPdf2), buFrac),
-      // deltaMean(("deltaMean_" + std::to_string(uniqueId)).c_str(), "", 143,
-      // 136,
-      //           160),
-      // deltaSigma(("deltaSigma_" + std::to_string(uniqueId)).c_str(), "", 2,
-      // 0,
-      //            10),
+      deltaMean(("deltaMean_" + std::to_string(uniqueId)).c_str(), "", 143, 136,
+                160),
+      deltaSigma(("deltaSigma_" + std::to_string(uniqueId)).c_str(), "", 2, 0,
+                 10),
       // deltaA1(("deltaA1_" + std::to_string(uniqueId)).c_str(), "", 1, 0, 5),
-      // deltaN1(("deltaN1_" + std::to_string(uniqueId)).c_str(), "", 10),
-      // deltaA2(("deltaA2_" + std::to_string(uniqueId)).c_str(), "", -1, -5,
-      // -0.00001), deltaN2(("deltaN2_" + std::to_string(uniqueId)).c_str(), "",
-      // 10), deltaFrac(("deltaFrac_" + std::to_string(uniqueId)).c_str(), "",
-      // 0.5, 0,
-      //           1),
-      // m(B): 5170 - 5370
-      deltaMean(("deltaMean_" + std::to_string(uniqueId)).c_str(), "",
-                1.4239e+02),
-      deltaSigma(("deltaSigma_" + std::to_string(uniqueId)).c_str(), "",
-                 1.6248e+00),
-      deltaA1(("deltaA1_" + std::to_string(uniqueId)).c_str(), "", 9.2873e-01),
       deltaN1(("deltaN1_" + std::to_string(uniqueId)).c_str(), "", 10),
-      deltaA2(("deltaA2_" + std::to_string(uniqueId)).c_str(), "", -6.5084e-01),
+      // deltaA2(("deltaA2_" + std::to_string(uniqueId)).c_str(), "", -1, -5,
+      //         -0.00001),
       deltaN2(("deltaN2_" + std::to_string(uniqueId)).c_str(), "", 10),
-      deltaFrac(("deltaFrac_" + std::to_string(uniqueId)).c_str(), "",
-                1.7763e-01),
+      deltaFrac(("deltaFrac_" + std::to_string(uniqueId)).c_str(), "", 0.5, 0,
+                1),
+      // m(B): 5230 - 5300
+      deltaA1(("deltaA1_" + std::to_string(uniqueId)).c_str(), "", 8.4308e-01),
+      deltaA2(("deltaA2_" + std::to_string(uniqueId)).c_str(), "", -6.5446e-01),
+      // m(B): 5170 - 5370
+      // deltaMean(("deltaMean_" + std::to_string(uniqueId)).c_str(), "",
+      //           1.4239e+02),
+      // deltaSigma(("deltaSigma_" + std::to_string(uniqueId)).c_str(), "",
+      //            1.6248e+00),
+      // deltaA1(("deltaA1_" + std::to_string(uniqueId)).c_str(),
+      // "", 9.2873e-01), deltaN1(("deltaN1_" +
+      // std::to_string(uniqueId)).c_str(), "", 10), deltaA2(("deltaA2_" +
+      // std::to_string(uniqueId)).c_str(), "", -6.5084e-01),
+      // deltaN2(("deltaN2_" + std::to_string(uniqueId)).c_str(), "", 10),
+      // deltaFrac(("deltaFrac_" + std::to_string(uniqueId)).c_str(), "",
+      //           1.7763e-01),
       // m(B): 5170 - 5220
       // deltaMean(("deltaMean_" + std::to_string(uniqueId)).c_str(),
       // "", 1.4248e+02), deltaSigma(("deltaSigma_" +
@@ -95,14 +100,15 @@ Model::Model(Configuration &config, int _uniqueId)
       //             -1, 1),
       // deltaBkgPdf(("deltaBkgPdf_" + std::to_string(uniqueId)).c_str(), "",
       //             config.deltaMass, deltaLambda),
-      // buBkgMean(("buBkgMean_" + std::to_string(uniqueId)).c_str(),
-      // "", 5.2445e+03, 5235, 5255), buBkgSigma(("buBkgSigma_" +
-      // std::to_string(uniqueId)).c_str(), "", 8.5073e+01, 75, 95), m(B): 5170
-      // - 5370
       buBkgMean(("buBkgMean_" + std::to_string(uniqueId)).c_str(), "",
-                5.2466e+03),
+                5.2445e+03, 5235, 5255),
       buBkgSigma(("buBkgSigma_" + std::to_string(uniqueId)).c_str(), "",
-                 8.1571e+01),
+                 8.5073e+01, 60, 95),
+      // m(B) 5170 - 5370
+      // buBkgMean(("buBkgMean_" + std::to_string(uniqueId)).c_str(), "",
+      //           5.2466e+03),
+      // buBkgSigma(("buBkgSigma_" + std::to_string(uniqueId)).c_str(), "",
+      //            8.1571e+01),
       // m(B): 5170 - 5220
       // buBkgMean(("buBkgMean_" + std::to_string(uniqueId)).c_str(),
       // "", 5.2474e+03), buBkgSigma(("buBkgSigma_" +
@@ -115,12 +121,16 @@ Model::Model(Configuration &config, int _uniqueId)
       deltaBkgC(("deltaBkgC_" + std::to_string(uniqueId)).c_str(), "",
                 1.0052e+01),
       // deltaBkgA(("deltaBkgA_" + std::to_string(uniqueId)).c_str(), "",
-      // -2.4211e+00, -5, 5), m(B): 5170 - 5370
-      deltaBkgA(("deltaBkgA_" + std::to_string(uniqueId)).c_str(), "",
-                -4.6769e+00),
+      //           -2.4211e+00, -5, 5),
+      // m(B): 5170 - 5370
+      // deltaBkgA(("deltaBkgA_" + std::to_string(uniqueId)).c_str(), "",
+      //           -4.6769e+00),
       // 5170 - 5220
       // deltaBkgA(("deltaBkgA_" + std::to_string(uniqueId)).c_str(), "",
       // -3.2607e+00),
+      // 5230 - 5300
+      deltaBkgA(("deltaBkgA_" + std::to_string(uniqueId)).c_str(), "",
+                2.0868e+00),
       deltaBkgB(("deltaBkgB_" + std::to_string(uniqueId)).c_str(), "",
                 2.6138e-01),
       deltaBkgPdf(("deltaBkgPdf_" + std::to_string(uniqueId)).c_str(), "",
@@ -189,6 +199,7 @@ Model::Model(Configuration &config, int _uniqueId)
   functions2D.add(bkgPdf2D);
   yields2D.add(N_Sig2D);
   yields2D.add(N_Bkg2D);
-  pdf2D = std::unique_ptr<RooAddPdf>(new RooAddPdf(
-      ("pdf2D_" + std::to_string(uniqueId)).c_str(), "", functions2D, yields2D));
+  pdf2D = std::unique_ptr<RooAddPdf>(
+      new RooAddPdf(("pdf2D_" + std::to_string(uniqueId)).c_str(), "",
+                    functions2D, yields2D));
 }
