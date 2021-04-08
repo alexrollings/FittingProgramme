014e9aed0588b652d5706875172ac3e8e25b87dd
Alexandra Rollings <alexandra.rollings@cern.ch>
2021-04-08 13:40:46 +0100

    Read in json file and key to remove in remove_json_key.py

--- results_analysis/remove_json_key.py
+++ results_analysis/remove_json_key.py
@@ -1,15 +1,29 @@
-import os, json
+import os, json, argparse
 
-json_fname = '/data/lhcb/users/rollings/systematics/9bf362c/gamma/systematics_gamma.json'
-if os.path.exists(json_fname):
-  with open(json_fname, 'r') as json_file:
-    json_dict = json.load(json_file)
-    key = 'Bs2D0Kst0_PdfDelta'
-    if key in json_dict:
-      json_dict.pop(key, None)
-      json_tmp = '/data/lhcb/users/rollings/systematics/9bf362c/gamma/systematics_gamma.json.tmp'
-      with open(json_tmp, 'w') as tmp:
-        json.dump(json_dict, tmp)
-      os.rename(json_tmp, json_fname)
-    else:
-      print(f'{key} not in {json_fname} dict')
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-f',
+                      '--json_fname',
+                      type=str,
+                      help='Name of json file',
+                      required=True)
+  parser.add_argument('-k',
+                      '--key',
+                      type=str,
+                      help='Key to remove (name of systematic label)',
+                      required=True)
+  args = parser.parse_args()
+  json_fname = args.json_fname
+  key = args.key
+
+  if os.path.exists(json_fname):
+    with open(json_fname, 'r') as json_file:
+      json_dict = json.load(json_file)
+      if key in json_dict:
+        json_dict.pop(key, None)
+        json_tmp = json_fname + '.tmp'
+        with open(json_tmp, 'w') as tmp:
+          json.dump(json_dict, tmp)
+        os.rename(json_tmp, json_fname)
+      else:
+        print(f'{key} not in {json_fname} dict')
