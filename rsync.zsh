9fea82ab343f7943bb2b785fcd89e3a98687ddbd
Alexandra Rollings <alexandra.rollings@cern.ch>
2021-04-29 13:29:48 +0100

    rsync fitting resources to eos

---
+++ rsync.zsh
@@ -0,0 +1,7 @@
+rsync -avzP /data/lhcb/users/rollings/data_fit/data_tuples/*.root arolling@lxplus.cern.ch:/eos/lhcb/wg/b2oc/ADS-GLW_Bu2Dst0h_FullReco_Run12/data_tuples 
+rsync -avzP /data/lhcb/users/rollings/data_fit/roodatasets/*.root arolling@lxplus.cern.ch:/eos/lhcb/wg/b2oc/ADS-GLW_Bu2Dst0h_FullReco_Run12/roodatasets
+rsync -avzP /data/lhcb/users/rollings/data_fit/mc_roofit_results/*.root arolling@lxplus.cern.ch:/eos/lhcb/wg/b2oc/ADS-GLW_Bu2Dst0h_FullReco_Run12/mc_roofit_results 
+rsync -avzP /data/lhcb/users/rollings/data_fit/fixed_rates/*.txt arolling@lxplus.cern.ch:/eos/lhcb/wg/b2oc/ADS-GLW_Bu2Dst0h_FullReco_Run12/fixed_rates 
+rsync -avzP /data/lhcb/users/rollings/data_fit/efficiencies/mcEffs*.txt arolling@lxplus.cern.ch:/eos/lhcb/wg/b2oc/ADS-GLW_Bu2Dst0h_FullReco_Run12/efficiencies/
+rsync -avzP /data/lhcb/users/rollings/data_fit/efficiencies/boxEffs*138_148_5220_5330.txt arolling@lxplus.cern.ch:/eos/lhcb/wg/b2oc/ADS-GLW_Bu2Dst0h_FullReco_Run12/efficiencies/
+rsync -avzP /data/lhcb/users/rollings/data_fit/efficiencies/boxEffs*60_105_125_170_5240_5320.txt arolling@lxplus.cern.ch:/eos/lhcb/wg/b2oc/ADS-GLW_Bu2Dst0h_FullReco_Run12/efficiencies/
