Fixed bug in comparison plots: dB should be in radians

results_analysis/compare_results.py
@@ -9,6 +9,7 @@ from useful_functions import return_label
 from uncertainties import ufloat
 import matplotlib
 matplotlib.use('Agg')
+from scipy.stats import mode
 
 rc('font',**{'family': 'serif','serif': ['Roman']})
 rc('text', usetex=True)
@@ -26,15 +27,16 @@ if os.path.exists(json_fname_result_gamma):
     result_gamma = json.load(json_file_result_gamma)
 
 # LHCb combination
-# phi3_av_err = (4.6 + 5.3)*0.5
-# phi3_val_err = ufloat(math.radians(71.1),math.radians(phi3_av_err))
 phi3_val_err = ufloat(math.radians(67),math.radians(4))
 phi3 = np.random.normal(phi3_val_err.n, phi3_val_err.s, 10000)
+# phi3 = math.radians(67)
 
 # No assumptions in D*π mode
 # B- -> D*0 π-
-rB_Dpi = np.random.uniform(0.0, 0.02, 10000)
-dB_Dpi = np.random.uniform(math.radians(0.0), math.radians(180.0), 10000)
+# rB_Dpi = np.random.uniform(0.0, 0.02, 10000)
+# rB_Dpi = np.random.uniform(0.0, 0.01, 10000)
+# dB_Dpi = np.random.uniform(math.radians(0.0), math.radians(180.0), 10000)
+# dB_Dpi = np.random.uniform(math.radians(0.0), math.radians(360.0), 10000)
 
 # B- -> D*0 K-
 # rB_DK = np.random.uniform(0.0, 0.2, 10000)
@@ -43,18 +45,26 @@ dB_Dpi = np.random.uniform(math.radians(0.0), math.radians(180.0), 10000)
 # dB_DK = np.random.normal(dB_DK_val_err.n, dB_DK_val_err.s, 10000)
 
 # B factory GGSZ
-rB_DK = np.random.normal(0.135, 0.034, 10000)
-dB_DK = np.random.normal(304, 17, 10000)
+# rB_DK = np.random.normal(0.135, 0.034, 10000)
+# rB_DK = 0.135
+# dB_DK = np.random.normal(math.radians(304), math.radians(17), 10000)
+# dB_DK = math.radians(304)
+
+# B factory ADS/GLW
+rB_DK = np.random.normal(0.106, 0.028, 10000)
+dB_DK = np.random.normal(math.radians(294), math.radians(26), 10000)
 
 # FAV: D0 -> K-π+
 rD_val = np.sqrt(0.00344)
 rD_err = rD_val * 2 * (2e-5/0.00344)
 rD_val_err = ufloat(rD_val,rD_err)
 rD = np.random.normal(rD_val_err.n, rD_val_err.s, 10000)
+# rD = np.sqrt(0.00344)
 
 dD_av_err = (8.6 + 10.2)*0.5
 dD_val_err = ufloat(math.radians(192.1),math.radians(dD_av_err))
 dD = np.random.normal(dD_val_err.n, dD_val_err.s, 10000)
+# dD = math.radians(192.1)
 
 #Calculate observables
 #Rates
@@ -62,16 +72,53 @@ pi0_DK_hh_plus = 1 + rB_DK**2 + 2 * rB_DK * np.cos(dB_DK + phi3)
 pi0_DK_hh_minus = 1 + rB_DK**2 + 2 * rB_DK * np.cos(dB_DK - phi3)
 gamma_DK_hh_plus = 1 + rB_DK**2 - 2 * rB_DK * np.cos(dB_DK + phi3)
 gamma_DK_hh_minus = 1 + rB_DK**2 - 2 * rB_DK * np.cos(dB_DK - phi3)
+# print(np.mean(pi0_DK_hh_plus))
+# fig = plt.hist(pi0_DK_hh_plus, 100, density=True)
+# plt.savefig('figs/pi0_DK_hh_plus.pdf')
+# plt.clf()
+# print(np.mean(pi0_DK_hh_minus))
+# fig = plt.hist(pi0_DK_hh_minus, 100, density=True)
+# plt.savefig('figs/pi0_DK_hh_minus.pdf')
+# plt.clf()
+# print(np.mean(gamma_DK_hh_plus))
+# fig = plt.hist(gamma_DK_hh_plus, 100, density=True)
+# plt.savefig('figs/gamma_DK_hh_plus.pdf')
+# plt.clf()
+# print(np.mean(gamma_DK_hh_minus))
+# fig = plt.hist(gamma_DK_hh_minus, 100, density=True)
+# plt.savefig('figs/gamma_DK_hh_minus.pdf')
+# plt.clf()
 
-pi0_Dpi_hh_plus = 1 + rB_Dpi**2 + 2 * rB_Dpi * np.cos(dB_Dpi + phi3)
-pi0_Dpi_hh_minus = 1 + rB_Dpi**2 + 2 * rB_Dpi * np.cos(dB_Dpi - phi3)
-gamma_Dpi_hh_plus = 1 + rB_Dpi**2 - 2 * rB_Dpi * np.cos(dB_Dpi + phi3)
-gamma_Dpi_hh_minus = 1 + rB_Dpi**2 - 2 * rB_Dpi * np.cos(dB_Dpi - phi3)
+# pi0_Dpi_hh_plus = 1 + rB_Dpi**2 + 2 * rB_Dpi * np.cos(dB_Dpi + phi3)
+# pi0_Dpi_hh_minus = 1 + rB_Dpi**2 + 2 * rB_Dpi * np.cos(dB_Dpi - phi3)
+# gamma_Dpi_hh_plus = 1 + rB_Dpi**2 - 2 * rB_Dpi * np.cos(dB_Dpi + phi3)
+# gamma_Dpi_hh_minus = 1 + rB_Dpi**2 - 2 * rB_Dpi * np.cos(dB_Dpi - phi3)
+# print(np.mean(pi0_Dpi_hh_plus))
+# fig = plt.hist(pi0_Dpi_hh_plus, 100, density=True)
+# plt.savefig('figs/pi0_Dpi_hh_plus.pdf')
+# plt.clf()
+# print(np.mean(pi0_Dpi_hh_minus))
+# fig = plt.hist(pi0_Dpi_hh_minus, 100, density=True)
+# plt.savefig('figs/pi0_Dpi_hh_minus.pdf')
+# plt.clf()
+# print(np.mean(gamma_Dpi_hh_plus))
+# fig = plt.hist(gamma_Dpi_hh_plus, 100, density=True)
+# plt.savefig('figs/gamma_Dpi_hh_plus.pdf')
+# plt.clf()
+# print(np.mean(gamma_Dpi_hh_minus))
+# fig = plt.hist(gamma_Dpi_hh_minus, 100, density=True)
+# plt.savefig('figs/gamma_Dpi_hh_minus.pdf')
+# plt.clf()
 
 pi0_DK_kpi_plus = 1 + rD**2 * rB_DK**2 + 2 * rD * rB_DK * np.cos(dB_DK - dD + phi3)
 pi0_DK_kpi_minus = 1 + rD**2 * rB_DK**2 + 2 * rD * rB_DK * np.cos(dB_DK - dD - phi3)
 gamma_DK_kpi_plus = 1 + rD**2 * rB_DK**2 - 2 * rD * rB_DK * np.cos(dB_DK - dD + phi3)
 gamma_DK_kpi_minus = 1 + rD**2 * rB_DK**2 - 2 * rD * rB_DK * np.cos(dB_DK - dD - phi3)
+
+# pi0_Dpi_kpi_plus = 1 + rD**2 * rB_Dpi**2 + 2 * rD * rB_Dpi * np.cos(dB_Dpi - dD + phi3)
+# pi0_Dpi_kpi_minus = 1 + rD**2 * rB_Dpi**2 + 2 * rD * rB_Dpi * np.cos(dB_Dpi - dD - phi3)
+# gamma_Dpi_kpi_plus = 1 + rD**2 * rB_Dpi**2 - 2 * rD * rB_Dpi * np.cos(dB_Dpi - dD + phi3)
+# gamma_Dpi_kpi_minus = 1 + rD**2 * rB_Dpi**2 - 2 * rD * rB_Dpi * np.cos(dB_Dpi - dD - phi3)
 
 pi0_DK_pik_plus = rB_DK**2 + rD**2 + 2 * rB_DK * rD * np.cos(dB_DK + dD + phi3)
 pi0_DK_pik_minus = rB_DK**2 + rD**2 + 2 * rB_DK * rD * np.cos(dB_DK + dD - phi3)
@@ -85,11 +132,41 @@ R_gamma_DK_pik_plus = gamma_DK_pik_plus / gamma_DK_kpi_plus
 
 #Observables
 exp = {}
-exp['R_CP_Bu2Dst0h_D0pi0']  = ((pi0_DK_hh_minus + pi0_DK_hh_plus)/(pi0_Dpi_hh_minus + pi0_Dpi_hh_plus))
-exp['R_CP_Bu2Dst0h_D0gamma'] = ((gamma_DK_hh_minus + gamma_DK_hh_plus)/(gamma_Dpi_hh_minus + gamma_Dpi_hh_plus))
-
+# exp['R_CP_Bu2Dst0h_D0pi0'] = ((pi0_DK_hh_minus + pi0_DK_hh_plus) /
+#                               (pi0_Dpi_hh_minus + pi0_Dpi_hh_plus)) / (
+#                                   (pi0_DK_kpi_minus + pi0_DK_kpi_plus) /
+#                                   (pi0_Dpi_kpi_minus + pi0_Dpi_kpi_plus))
+exp['R_CP_Bu2Dst0h_D0pi0'] = ((pi0_DK_hh_minus + pi0_DK_hh_plus) /
+                                  (pi0_DK_kpi_minus + pi0_DK_kpi_plus))
+print(np.mean(exp['R_CP_Bu2Dst0h_D0pi0']))
+print(np.std(exp['R_CP_Bu2Dst0h_D0pi0']))
+# exp['R_CP_Bu2Dst0h_D0gamma'] = ((gamma_DK_hh_minus + gamma_DK_hh_plus) /
+#                               (gamma_Dpi_hh_minus + gamma_Dpi_hh_plus)) / (
+#                                   (gamma_DK_kpi_minus + gamma_DK_kpi_plus) /
+#                                   (gamma_Dpi_kpi_minus + gamma_Dpi_kpi_plus))
+exp['R_CP_Bu2Dst0h_D0gamma'] = ((gamma_DK_hh_minus + gamma_DK_hh_plus) /
+                                  (gamma_DK_kpi_minus + gamma_DK_kpi_plus))
+print(np.mean(exp['R_CP_Bu2Dst0h_D0gamma']))
+print(np.std(exp['R_CP_Bu2Dst0h_D0gamma']))
 exp['R_piK_Bu2Dst0h_D0pi0_k_total'] = (R_pi0_DK_pik_minus + R_pi0_DK_pik_plus) / 2
+print(np.mean(exp['R_piK_Bu2Dst0h_D0pi0_k_total']))
+print(np.std(exp['R_piK_Bu2Dst0h_D0pi0_k_total']))
 exp['R_piK_Bu2Dst0h_D0gamma_k_total'] = (R_gamma_DK_pik_minus + R_gamma_DK_pik_plus) / 2
+print(np.mean(exp['R_piK_Bu2Dst0h_D0gamma_k_total']))
+print(np.std(exp['R_piK_Bu2Dst0h_D0gamma_k_total']))
+
+fig = plt.hist(exp['R_CP_Bu2Dst0h_D0pi0'], 100, density=True)
+plt.savefig('figs/R_CP_pi0.pdf')
+plt.clf()
+fig = plt.hist(exp['R_CP_Bu2Dst0h_D0gamma'], 100, density=True)
+plt.savefig('figs/R_CP_gamma.pdf')
+plt.clf()
+fig = plt.hist(exp['R_piK_Bu2Dst0h_D0pi0_k_total'], 100, density=True)
+plt.savefig('figs/R_ADS_pi0.pdf')
+plt.clf()
+fig = plt.hist(exp['R_piK_Bu2Dst0h_D0gamma_k_total'], 100, density=True)
+plt.savefig('figs/R_ADS_gamma.pdf')
+plt.clf()
 
 # Part-reco results
 part_reco = {}
@@ -121,6 +198,11 @@ part_reco['R_piK_Bu2Dst0h_D0gamma_k_total'] = {'Value' : 0.0163, 'Error' : 0.037
 # print(b_factory)
 
 pars = ['R_CP_Bu2Dst0h_D0pi0', 'R_CP_Bu2Dst0h_D0gamma', 'R_piK_Bu2Dst0h_D0pi0_k_total', 'R_piK_Bu2Dst0h_D0gamma_k_total']
+
+for p in pars:
+  exp_mu = np.mean(exp[p])
+  exp_sigma = np.std(exp[p])
+  print(f'{p}: {exp_mu:.4f} +/- {exp_sigma:.4f}')
 
 # Scale stat error to include syst
 syst = {
@@ -182,6 +264,7 @@ for p in pars:
   high5 = exp_mu + 5*exp_sigma
   low6 = exp_mu - 6*exp_sigma
   high6 = exp_mu + 6*exp_sigma
+  left_text = exp_mu + 5.3*exp_sigma
 
   plt.axvline(x=exp_mu,color='black',linestyle='--',label='Expected')
   plt.axvspan(low, high, color='#4575b4',label='68\% C.L.')
@@ -193,32 +276,32 @@ for p in pars:
   span = high6 - low6
   plt.xlim(low6,high6 + span/2)
 
-  plt.text(high5,2.4,"PR Analysis",fontsize=18)
+  plt.text(left_text,2.4,"PR Analysis",fontsize=18)
   if 'R_CP' in p:
-    plt.text(high5,2.0,f"{part_reco_val:.3f} $\\pm$ {part_reco_err:.3f}",fontsize=18)
+    plt.text(left_text,2.0,f"{part_reco_val:.3f} $\\pm$ {part_reco_err:.3f}",fontsize=18)
   else:
-    plt.text(high5,2.0,f"{part_reco_val:.4f} $\\pm$ {part_reco_err:.4f}",fontsize=18)
-  plt.text(high5,1.5,f"[LHCb-PAPER-2020-036]",fontsize=18)
+    plt.text(left_text,2.0,f"{part_reco_val:.4f} $\\pm$ {part_reco_err:.4f}",fontsize=18)
+  plt.text(left_text,1.5,f"[LHCb-PAPER-2020-036]",fontsize=18)
   if p in result_gamma:
     if 'pi0' in p:
-      plt.text(high5,4.2,"$D^{*}\\rightarrow D[\\gamma]_{\\pi^{0}}$",fontsize=18)
+      plt.text(left_text,4.2,"$D^{*}\\rightarrow D[\\gamma]_{\\pi^{0}}$",fontsize=18)
     else:
-      plt.text(high5,4.2,"$D^{*}\\rightarrow D\\gamma$",fontsize=18)
+      plt.text(left_text,4.2,"$D^{*}\\rightarrow D\\gamma$",fontsize=18)
     if 'R_CP' in p:
-      plt.text(high5,3.8,f"{gamma_val:.3f} $\\pm$ {gamma_err:.3f}",fontsize=18)
+      plt.text(left_text,3.8,f"{gamma_val:.3f} $\\pm$ {gamma_err:.3f}",fontsize=18)
     else:
-      plt.text(high5,3.8,f"{gamma_val:.4f} $\\pm$ {gamma_err:.4f}",fontsize=18)
+      plt.text(left_text,3.8,f"{gamma_val:.4f} $\\pm$ {gamma_err:.4f}",fontsize=18)
   if p in result_pi0:
-    plt.text(high5,6.2,"$D^{*}\\rightarrow D\\pi^{0}$",fontsize=18)
+    plt.text(left_text,6.2,"$D^{*}\\rightarrow D\\pi^{0}$",fontsize=18)
     if 'R_CP' in p:
-      plt.text(high5,5.8,f"{pi0_val:.3f} $\\pm$ {pi0_err:.3f}",fontsize=18)
+      plt.text(left_text,5.8,f"{pi0_val:.3f} $\\pm$ {pi0_err:.3f}",fontsize=18)
     else:
-      plt.text(high5,5.8,f"{pi0_val:.4f} $\\pm$ {pi0_err:.4f}",fontsize=18)
+      plt.text(left_text,5.8,f"{pi0_val:.4f} $\\pm$ {pi0_err:.4f}",fontsize=18)
 
 
   if p_gamma == True:
-    plt.text(high5, 7.2,'$B$-factories GGSZ analyses:',fontsize=18)
-    plt.legend(loc=(0.6, 0.65), fontsize=18,frameon=False)
+    plt.text(left_text, 7.2,'$B$-factory measurements:',fontsize=18)
+    plt.legend(loc=(0.62, 0.65), fontsize=18,frameon=False)
 
   plt.ylim(0.0,8.0)
   plt.xlabel(return_label(p),fontsize=25,labelpad=10)