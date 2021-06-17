6e0af5fc1744078a9a9986b57bc9af7e7524f01f
Alexandra Rollings <alexandra.rollings@cern.ch>
2021-06-17 13:54:25 +0100

    Use weights DS for B2D0Kst too

--- src/Params.cpp
+++ src/Params.cpp
@@ -111,11 +111,11 @@ double Params::ReturnValErr(Mode mode, Neutral neutral, Bachelor bachelor,
   if (bachelor == Bachelor::k) {
     bachStr = "K";
   }
-  std::string fname =
-      Configuration::Get().inputDir + "/mc_roofit_results/MC_" +
-      EnumToString(neutral) + "_" + EnumToString(mode) + "_" +
-      bachStr;
-  if (mode == Mode::Bu2D0rho && bachelor == Bachelor::pi) {
+  std::string fname = Configuration::Get().inputDir + "/mc_roofit_results/MC_" +
+                      EnumToString(neutral) + "_" + EnumToString(mode) + "_" +
+                      bachStr;
+  if ((mode == Mode::Bu2D0rho && bachelor == Bachelor::pi) ||
+      (mode == Mode::Bu2D0Kst && bachelor == Bachelor::K)) {
     fname += "_weights";
   }
   fname += ".root";
