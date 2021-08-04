e5bd0c92f883bdeded9e0418a994e94533bb9bd7
Alexandra Rollings <alexandra.rollings@cern.ch>
2021-08-04 11:48:14 +0100

    Changed name of constraints argset

--- src/BachelorVars.cpp
+++ src/BachelorVars.cpp
@@ -261,7 +261,7 @@ BachelorVars<_bachelor>::BachelorVars(int uniqueId)
       A_CP_Bu2D0hst_gaus_ = std::unique_ptr<RooGaussian>(new RooGaussian(
           ("A_CP_Bu2D0hst_gaus_" + ComposeName(uniqueId_, _bachelor)).c_str(),
           "", *A_CP_Bu2D0hst_, RooFit::RooConst(0.06), RooFit::RooConst(0.09)));
-      GlobalVars::Get(uniqueId_).constraints_argset().add(*A_CP_Bu2D0hst_gaus_);
+      GlobalVars::Get(uniqueId_).constraints_argSet().add(*A_CP_Bu2D0hst_gaus_);
     }
   }
 }
