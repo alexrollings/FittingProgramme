fb6ee8fd3c0cc0422b6af2c44d4724e80a638f25
Alexandra Rollings <alexandra.rollings@cern.ch>
2021-08-05 10:11:55 +0100

    Set toys seed to rndm in fitting

--- src/Fitting.cpp
+++ src/Fitting.cpp
@@ -822,7 +822,7 @@ int main(int argc, char **argv) {
       std::default_random_engine rng(rd());
       std::uniform_int_distribution<UInt_t> dist;
       UInt_t seed = dist(rng);
-      // UInt_t seed = 0xad60f73b;
+      // UInt_t seed = 0x49affb74;
       RooRandom::randomGenerator()->SetSeed(seed);
       std::stringstream filename;
       if (config.runToy() == true && pdfD1D == true) {
