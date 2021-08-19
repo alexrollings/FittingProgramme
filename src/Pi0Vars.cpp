a26df9d367948a8c57c049f87e899df779e1b7f2
Alexandra Rollings <alexandra.rollings@cern.ch>
2021-08-19 10:13:23 +0100

    No adjustment for deltaEffBs2D0Kst0_ in Pi0Vars

--- src/Pi0Vars.cpp
+++ src/Pi0Vars.cpp
@@ -498,8 +498,7 @@ NeutralVars<Neutral::pi0>::NeutralVars(int uniqueId)
       deltaEffBs2D0Kst0_(Params::Get().CreateFixed(
           "deltaEffBs2D0Kst0", uniqueId_, Neutral::pi0,
           ReturnBoxEffs(Mode::Bs2D0Kst0, Bachelor::k, Efficiency::deltaEff,
-                        false) +
-              0.0007,
+                        false),
           ReturnBoxEffs(Mode::Bs2D0Kst0, Bachelor::k, Efficiency::deltaEffErr,
                         false),
           Systematic::boxEffs_Bs2D0Kst0, Sign::same)),
